cpinte/mcfost

commit aa6463f8331da8636a4382947c0a7e57567be7cf

Updated wavelength grid for ProDiMo
- new lambda file
- bins are now touching each other in prodimo mode

src/grid.f90
@@ -1118,29 +1118,31 @@ end subroutine indice_cellule_3D
 subroutine init_lambda()
   ! Initialisation table de longueurs d'onde
 
-  implicit none
-
-  real :: facteur_largeur
   integer :: i
 
   if (lmono0) then
      ! Lecture longueur d'onde
      read(band,*) tab_lambda(1)
      tab_delta_lambda(1) = 1.0
+     tab_lambda_inf(1) = tab_lambda(1)
+     tab_lambda_sup(1) = tab_lambda(1)
 
   else
      ! Initialisation longueurs d'onde
      !delta_lambda = (lambda_max/lambda_min)**(1.0/real(n_lambda))
-     delta_lambda =  exp((1.0_db/real(n_lambda,kind=db)) * log(lambda_max/lambda_min))
+     delta_lambda =  exp( (1.0_db/real(n_lambda,kind=db)) * log(lambda_max/lambda_min) )
      
+     tab_lambda_inf(1) = lambda_min
      tab_lambda(1)=lambda_min*sqrt(delta_lambda)
+     tab_lambda_sup(1) = lambda_min*delta_lambda
      do i=2, n_lambda
         tab_lambda(i)= tab_lambda(i-1)*delta_lambda
+        tab_lambda_sup(i)= tab_lambda_sup(i-1)*delta_lambda
+        tab_lambda_inf(i)= tab_lambda_sup(i-1)        
      enddo
      
-     facteur_largeur=sqrt(delta_lambda)-1.0/sqrt(delta_lambda)
      do i=1, n_lambda
-        tab_delta_lambda(i) = tab_lambda(i) * facteur_largeur
+        tab_delta_lambda(i) = tab_lambda_sup(i) - tab_lambda_inf(i)
      enddo
 
   endif
@@ -1162,9 +1164,10 @@ subroutine init_lambda2()
   n_lambda=n_lambda2
   do i=1, n_lambda2
      tab_lambda(i)= tab_lambda2(i)
+     tab_lambda_inf(i)= tab_lambda2_inf(i)
+     tab_lambda_sup(i)= tab_lambda2_sup(i)
      tab_delta_lambda(i)= tab_delta_lambda2(i)
   enddo
-  
 
   return
   

src/input.f90
@@ -474,6 +474,8 @@ subroutine lect_lambda()
   integer :: alloc_status, lambda, status, n_comment, i
   real :: fbuffer
 
+  real, parameter :: wl_factor = 1.025
+
   lambda_filename = trim(lambda_dir)//trim(tab_wavelength)
   open(unit=1,file=lambda_filename,status="old",iostat=status)
   if (status /= 0) then
@@ -517,20 +519,43 @@ subroutine lect_lambda()
   enddo
 
   ! Allocations des tab
-  allocate(tab_lambda2(n_lambda2), tab_delta_lambda2(n_lambda2), stat=alloc_status)
+  allocate(tab_lambda2(n_lambda2), tab_lambda2_inf(n_lambda2),tab_lambda2_sup(n_lambda2), tab_delta_lambda2(n_lambda2), stat=alloc_status)
   if (alloc_status > 0) then
      write(*,*) 'Allocation error tab_lambda2'
      stop
   endif
-  tab_lambda2 = 0.
-  tab_delta_lambda2 = 1.
 
   ! Lecture proprement dite
   do lambda=1, n_lambda2
      read(1,*) tab_lambda2(lambda)!, tab_delta_lambda2(lambda)
+     tab_lambda2_inf(lambda) = tab_lambda2(lambda) / wl_factor
+     tab_lambda2_sup(lambda) = tab_lambda2(lambda) * wl_factor
+     tab_delta_lambda2(lambda) = tab_lambda2_sup(lambda) - tab_lambda2_inf(lambda)
   enddo
   close(unit=1)
-  tab_delta_lambda2(:) = 0.05 * tab_lambda2(:)
+
+
+  if (lProDiMo) then
+     write(*,*) "WARNING:  matching step2 wavelength bins to ProDiMo set-up"
+     tab_lambda2_inf(1) = 0.0912
+     tab_lambda2_sup(1) = tab_lambda2(1) * tab_lambda2(1)/tab_lambda2_inf(1)
+     tab_delta_lambda2(1) = tab_lambda2_sup(1) - tab_lambda2_inf(1)
+     do lambda=2, n_lambda2
+        tab_lambda2_inf(lambda) = tab_lambda2_sup(lambda-1)
+        tab_lambda2_sup(lambda) = tab_lambda2(lambda)  * tab_lambda2(lambda)/tab_lambda2_inf(lambda)
+        tab_delta_lambda2(lambda) = tab_lambda2_sup(lambda) - tab_lambda2_inf(lambda)
+     enddo
+
+     do lambda=1, n_lambda2
+        if (tab_delta_lambda2(lambda) <= 0.0) then
+           write(*,*) "ERROR in ProDiMo wavelength grid."
+           write(*,*) "Exiting"
+           write(*,*) lambda, "wl=", tab_lambda2(lambda), "delta=", tab_delta_lambda2(lambda)
+           write(*,*) "            min, max =", tab_lambda2_inf(lambda), tab_lambda2_sup(lambda)
+           stop
+        endif
+     enddo
+  endif ! lProDiMo
 
   return
 

src/mem.f90
@@ -258,12 +258,12 @@ subroutine alloc_dynamique()
   spectre_etoiles = 0.0
   spectre_emission_cumul = 0.0
 
-  allocate(tab_lambda(n_lambda), tab_delta_lambda(n_lambda), stat=alloc_status)
+  allocate(tab_lambda(n_lambda), tab_lambda_inf(n_lambda), tab_lambda_sup(n_lambda), tab_delta_lambda(n_lambda), stat=alloc_status)
   if (alloc_status > 0) then
      write(*,*) 'Allocation error tab_lambda'
      stop
   endif
-  tab_lambda=0.0; tab_delta_lambda=0.0
+  tab_lambda=0.0 ;  tab_lambda_inf=0.0 ; tab_lambda_sup=0.0 ; tab_delta_lambda=0.0
 
   allocate(tab_amu1(n_lambda, n_pop), tab_amu2(n_lambda, n_pop), stat=alloc_status)
   if (alloc_status > 0) then
@@ -996,7 +996,7 @@ subroutine dealloc_em_th()
 
   deallocate(spectre_etoiles_cumul,spectre_etoiles, spectre_emission_cumul)
 
-  deallocate(tab_lambda,tab_delta_lambda,tab_amu1,tab_amu2)
+  deallocate(tab_lambda,tab_lambda_inf,tab_lambda_sup,tab_delta_lambda,tab_amu1,tab_amu2)
 
   deallocate(amax_reel,kappa,kappa_abs_eg,proba_abs_RE)
 
@@ -1055,13 +1055,13 @@ subroutine realloc_dust_mol()
 
   integer :: alloc_status
 
-  allocate(tab_lambda(n_lambda), tab_delta_lambda(n_lambda), &
+  allocate(tab_lambda(n_lambda), tab_lambda_inf(n_lambda), tab_lambda_sup(n_lambda), tab_delta_lambda(n_lambda), &
        tab_amu1(n_lambda, n_pop), tab_amu2(n_lambda, n_pop), stat=alloc_status)
   if (alloc_status > 0) then
      write(*,*) 'Allocation error tab_lambda (realloc)'
      stop
   endif
-  tab_lambda=0.0 ; tab_delta_lambda= 0.0 ; tab_amu1=0.0 ; tab_amu2=0.0
+  tab_lambda=0.0 ; tab_lambda_inf = 0.0 ; tab_lambda_sup = 0.0 ; tab_delta_lambda= 0.0 ; tab_amu1=0.0 ; tab_amu2=0.0
 
   allocate(tab_albedo(n_lambda,n_grains_tot), stat=alloc_status)
   if (alloc_status > 0) then
@@ -1190,7 +1190,7 @@ subroutine clean_mem_dust_mol()
   integer :: alloc_status
 
   ! Ne reste que kappa et emissivite_dust
-  deallocate(tab_lambda, tab_delta_lambda, tab_amu1, tab_amu2)
+  deallocate(tab_lambda, tab_lambda_inf, tab_lambda_sup, tab_delta_lambda, tab_amu1, tab_amu2)
   deallocate(tab_albedo)
   deallocate(q_ext, q_sca, q_abs, tab_g)
   deallocate(prob_s11,tab_s11,tab_s12,tab_s33,tab_s34,probsizecumul)
@@ -1232,17 +1232,15 @@ subroutine realloc_step2()
   endif
 
   ! Liberation memoire step1 et reallocation step 2
-  deallocate(tab_lambda,tab_delta_lambda,tab_amu1,tab_amu2)
-  allocate(tab_lambda(n_lambda2),tab_delta_lambda(n_lambda2),tab_amu1(n_lambda2,n_pop), &
-       tab_amu2(n_lambda2,n_pop), stat=alloc_status)
+  deallocate(tab_lambda,tab_lambda_inf,tab_lambda_sup,tab_delta_lambda,tab_amu1,tab_amu2)
+  allocate(tab_lambda(n_lambda),tab_lambda_inf(n_lambda),tab_lambda_sup(n_lambda),tab_delta_lambda(n_lambda2),&
+       tab_amu1(n_lambda2,n_pop),tab_amu2(n_lambda2,n_pop), stat=alloc_status)
   if (alloc_status > 0) then
      write(*,*) 'Allocation error tab_lambda in init_lambda2'
      stop
   endif
-  tab_lambda=0.0
-  tab_delta_lambda=0.0
-  tab_amu1=0.0
-  tab_amu2=0.0
+  tab_lambda=0.0 ; tab_lambda_inf = 0.0 ; tab_lambda_sup = 0.0 ; tab_delta_lambda=0.0
+  tab_amu1=0.0 ; tab_amu2=0.0
 
   deallocate(n_phot_sed2)
   allocate(n_phot_sed2(nb_proc,n_lambda2,N_thet,N_phi), stat=alloc_status)

src/modules.f90
@@ -403,10 +403,10 @@ module grains
   real :: wavel
 
   ! Tab de lambda
-  real, dimension(:), allocatable :: tab_lambda, tab_delta_lambda !n_lambda
+  real, dimension(:), allocatable :: tab_lambda, tab_lambda_inf, tab_lambda_sup, tab_delta_lambda !n_lambda
   real, dimension (:,:), allocatable :: tab_amu1, tab_amu2 !n_lambda,n_pop
   real, dimension (:,:), allocatable :: tab_amu1_coating, tab_amu2_coating !n_lambda,n_pop
-  real, dimension(:), allocatable :: tab_lambda2, tab_delta_lambda2
+  real, dimension(:), allocatable :: tab_lambda2, tab_lambda2_inf, tab_lambda2_sup, tab_delta_lambda2
 
   ! Parametres de diffusion des grains
   real, dimension(:,:,:), allocatable :: tab_s11, tab_s12, tab_s33, tab_s34 !n_lambda,n_grains,180

src/init_mcfost.f90
@@ -660,8 +660,8 @@ subroutine initialisation_mcfost()
   endif
 
   if (lProDiMo) then
-     if ((lsed_complete).or.(tab_wavelength /= prodimo_tab_wavelength)) then
-        write(*,*) "WARNING: ProDiMo mode, forcing the wavelength grid using ProDiMo.lambda" 
+     if ((lsed_complete).or.(tab_wavelength(1:7) /= "ProDiMo")) then
+        write(*,*) "WARNING: ProDiMo mode, forcing the wavelength grid using ProDiMo_UV3_9.lambda" 
         lsed_complete = .false.       
         tab_wavelength = prodimo_tab_wavelength
      endif

src/io_prodimo.f90
@@ -32,7 +32,7 @@ module ProDiMo
   character(len=10) :: sProDiMo_fPAH
   
   ! Grille de longeurs d'onde
-  character(len=32) :: ProDiMo_tab_wavelength = "ProDiMo.lambda"
+  character(len=32) :: ProDiMo_tab_wavelength = "ProDiMo_UV3_9.lambda"
 
   ! Pour champ ISM
   real, parameter :: Wdil =  9.85357e-17 

src/scattering.f90
@@ -903,8 +903,8 @@ subroutine mueller_PAH(lambda,p_lambda,taille_grain,qext,qsca,gsca)
 
   ! Moyennage en longueur d'onde
   ! tableau decroissant
-  wl_min = tab_lambda(lambda) - 0.5 * tab_delta_lambda(lambda)
-  wl_max = tab_lambda(lambda) + 0.5 * tab_delta_lambda(lambda)
+  wl_min = tab_lambda_inf(lambda)
+  wl_max = tab_lambda_sup(lambda)
 
   qext = 0.0 ; qsca = 0.0 ; gsca = 0.0 ; norme = 0 ; N=0
   do i=1,PAH_n_lambda

src/stars.f90
@@ -751,8 +751,8 @@ subroutine repartition_energie_etoiles()
      spectre0 = 0.0 ;
      prob_E_star(lambda,0) = 0.0
         
-     wl_inf =  tab_lambda(lambda) - tab_delta_lambda(lambda)/2
-     wl_sup =  tab_lambda(lambda) + tab_delta_lambda(lambda)/2
+     wl_inf =  tab_lambda_inf(lambda)
+     wl_sup =  tab_lambda_sup(lambda)
 
      do i=1,n_etoiles
         surface=4*pi*(etoile(i)%r**2)

commit e579a54456f2686ce010316ee7a60306e1e40359

Bug fix: allocation error in tab_lambda step2
- tab_lambda2 allocated with dimension n_lambda and not n_lambda2 (bug introduced in aa6463f8331da8636a4382947c0a7e57567be7cf)
- allocation tests added in dust.f90

src/dust.f90
@@ -151,12 +151,17 @@ subroutine init_indices_optiques()
   do pop=1, n_pop
      if (.not.dust_pop(pop)%is_PAH) then 
 
-        allocate(tab_tmp_amu1(n_lambda, dust_pop(pop)%n_components), tab_tmp_amu2(n_lambda, dust_pop(pop)%n_components))
+        allocate(tab_tmp_amu1(n_lambda, dust_pop(pop)%n_components), &
+             tab_tmp_amu2(n_lambda, dust_pop(pop)%n_components), stat=alloc_status)
+        if (alloc_status > 0) then
+           write(*,*) 'Allocation error tab_tmp_amu1'
+           stop
+        endif
+        tab_tmp_amu1 = 0. ; tab_tmp_amu2 = 0.
 
         ! Lecture fichier indices
         do k=1, dust_pop(pop)%n_components
            filename = trim(dust_dir)//trim(dust_pop(pop)%indices(k))
-
            open(unit=1,file=filename, status='old', iostat=ios)
            if (ios /=0) then
               write(*,*) "ERROR: dust file cannot be opened:",trim(filename)
@@ -183,16 +188,16 @@ subroutine init_indices_optiques()
            n_ind = n_ind - 1
            
            ! On enleve les 2 premieres lignes
-           n_ind = n_ind - 2 
-        
+           n_ind = n_ind - 2
+
            ! Allocation de tab
            allocate(tab_l(n_ind), tab_n(n_ind), tab_k(n_ind), stat=alloc_status)
            if (alloc_status > 0) then
               write(*,*) 'Allocation error zsup'
               stop
            endif
-           tab_l=0.0 ; tab_n=0.0 ; tab_k=0.0 
-                   
+           tab_l=0.0 ; tab_n=0.0 ; tab_k=0.0
+
            ! Lecture proprement dite
            rewind(1)
            ! On passe les commentaires

src/grid.f90
@@ -1140,7 +1140,7 @@ subroutine init_lambda2()
   integer :: i
   
   ! reorganisation memoire
-  call realloc_step2
+  call realloc_step2()
   
   n_lambda=n_lambda2
   do i=1, n_lambda2

src/input.f90
@@ -519,7 +519,8 @@ subroutine lect_lambda()
   enddo
 
   ! Allocations des tab
-  allocate(tab_lambda2(n_lambda2), tab_lambda2_inf(n_lambda2),tab_lambda2_sup(n_lambda2), tab_delta_lambda2(n_lambda2), stat=alloc_status)
+  allocate(tab_lambda2(n_lambda2), tab_lambda2_inf(n_lambda2),tab_lambda2_sup(n_lambda2), &
+       tab_delta_lambda2(n_lambda2), stat=alloc_status)
   if (alloc_status > 0) then
      write(*,*) 'Allocation error tab_lambda2'
      stop

src/mem.f90
@@ -1233,7 +1233,7 @@ subroutine realloc_step2()
 
   ! Liberation memoire step1 et reallocation step 2
   deallocate(tab_lambda,tab_lambda_inf,tab_lambda_sup,tab_delta_lambda,tab_amu1,tab_amu2)
-  allocate(tab_lambda(n_lambda),tab_lambda_inf(n_lambda),tab_lambda_sup(n_lambda),tab_delta_lambda(n_lambda2),&
+  allocate(tab_lambda(n_lambda2),tab_lambda_inf(n_lambda2),tab_lambda_sup(n_lambda2),tab_delta_lambda(n_lambda2),&
        tab_amu1(n_lambda2,n_pop),tab_amu2(n_lambda2,n_pop), stat=alloc_status)
   if (alloc_status > 0) then
      write(*,*) 'Allocation error tab_lambda in init_lambda2'

src/modules.f90
@@ -7,7 +7,7 @@ module parametres
   save
 
   real, parameter :: mcfost_version = 2.14
-  character(8), parameter :: mcfost_release = "2.14.1"
+  character(8), parameter :: mcfost_release = "2.14.2"
   !character(len=128), parameter :: webpage="http://www-laog.obs.ujf-grenoble.fr/public/pintec/mcfost/"
   character(len=128), parameter :: webpage=      "http://ipag.osug.fr/public/pintec/mcfost/"
   character(len=128), parameter :: utils_webpage="http://ipag.osug.fr/public/pintec/mcfost_utils/"