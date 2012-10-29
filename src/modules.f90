3d98cdcfa474215ada72ac7541fc9bcc3cc3a0e5
Christophe Pinte <christophe.pinte@obs.ujf-grenoble.fr>
2012-10-29 17:14:54 +0100

    Global variables Rmax and map_size, removing size_neb  - size_neb is removed, replaced by a variable map_size valid for all zones  - Rout variable removed, replaced by Rmax: it allows for general grids, eg tappered-edge discs

--- src/modules.f90
+++ src/modules.f90
@@ -6,8 +6,8 @@ module parametres
   implicit none
   save
 
-  real, parameter :: mcfost_version = 2.14
-  character(8), parameter :: mcfost_release = "2.14.5"
+  real, parameter :: mcfost_version = 2.15
+  character(8), parameter :: mcfost_release = "2.15.0"
   !character(len=128), parameter :: webpage="http://www-laog.obs.ujf-grenoble.fr/public/pintec/mcfost/"
   character(len=128), parameter :: webpage=      "http://ipag.osug.fr/public/pintec/mcfost/"
   character(len=128), parameter :: utils_webpage="http://ipag.osug.fr/public/pintec/mcfost_utils/"
@@ -161,8 +161,6 @@ module disk
   implicit none
   save
 
-!*     size_neb est la dimension maxi de la nebuleuse spherique
-! Ne pas modifier par n_rad
   real :: distance ! Distance du disque en pc
 
 !* Parametres du disque
@@ -172,26 +170,25 @@ module disk
 !* en masse solaire
 !*     exp_beta.......... exposant decrivant le "flaring"
 !*     surf.......... exposant de la distribution SURFACIQUE de densite
-!*                    surf .neq. -2.0 !!!!!
 !*     rin, rout..... limites interne et externe du disque
 !*     rref.......... rayon de reference ("r0")
 !*     sclht......... echelle de hauteur a r0 ("h0")
 !*
-  real(kind=db) :: size_neb
+  real(kind=db) :: map_size
   
   type disk_zone_type 
-     real(kind=db) :: Rin, Rmin, Rc, Rout, Rref, edge, exp_beta, surf, sclht, diskmass
+     real(kind=db) :: Rin, Rmin, Rc, Rout, Rmax, Rref, edge, exp_beta, surf, sclht, diskmass
      integer :: geometry ! 1=disk, 2=tappered-disk, 3=envelope
   end type disk_zone_type
 
   type cavity_type
-     real(kind=db) ::  exp_beta, sclht, rref
+     real(kind=db) ::  exp_beta, sclht, Rref
   end type cavity_type
 
   type(disk_zone_type), dimension(:), allocatable, target :: disk_zone
   type(cavity_type) :: cavity
 
-  real(kind=db) :: rmin, rout, rout2, diskmass, correct_Rsub
+  real(kind=db) :: Rmin, Rmax, Rmax2, diskmass, correct_Rsub
 
   real :: r_subdivide
   logical :: lr_subdivide
