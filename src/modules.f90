cb874370af764540a0d2067fff59505773a857a3
Christophe Pinte <christophe.pinte@obs.ujf-grenoble.fr>
2016-10-25 12:08:53 +1100

    Adding test for cavity and parameter version number

--- src/modules.f90
+++ src/modules.f90
@@ -7,7 +7,7 @@ module parametres
   save
 
   real, parameter :: mcfost_version = 3.0
-  character(8), parameter :: mcfost_release = "3.0.6"
+  character(8), parameter :: mcfost_release = "3.0.7"
   real, parameter :: required_utils_version = 3.0
 
   character(len=128), parameter :: webpage=      "http://ipag.osug.fr/public/pintec/mcfost/"
