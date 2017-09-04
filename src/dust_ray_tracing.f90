cpinte/mcfost

commit f966c3f8ecf02db51fb1de07c6369bf76dbceafa

Changing default orientation of ray-traced images and mol maps
At i=0, the (x,y) image plane matches the (x,y) plane of the model.
Then the inclination is performed along the x axis, so that at 90 deg
inclination, the image plane correspond to the (x,z) model plane.

Re-writing find_pixel() routine

src/dust_ray_tracing.f90
@@ -254,8 +254,8 @@ subroutine init_directions_ray_tracing()
      tab_uv_rt(ibin) = sin(max(tab_RT_incl(ibin),1e-20)*deg_to_rad) ! uv_rt mais v_rt = 0 ici
      tab_w_rt(ibin) = sqrt(1.0_dp - tab_uv_rt(ibin)*tab_uv_rt(ibin))
      do iaz =1, RT_n_az
-        tab_u_rt(ibin,iaz) =   tab_uv_rt(ibin) * cos(tab_RT_az(iaz)*deg_to_rad)
-        tab_v_rt(ibin,iaz) =   tab_uv_rt(ibin) * sin(tab_RT_az(iaz)*deg_to_rad)
+        tab_u_rt(ibin,iaz) =   tab_uv_rt(ibin) * sin(tab_RT_az(iaz)*deg_to_rad)
+        tab_v_rt(ibin,iaz) =   - tab_uv_rt(ibin) * cos(tab_RT_az(iaz)*deg_to_rad)
      enddo
   enddo
 

src/dust_transfer.f90
@@ -1174,7 +1174,7 @@ subroutine dust_map(lambda,ibin,iaz)
   ! Definition des vecteurs de base du plan image dans le repere universel
 
   ! Vecteur x image sans PA : il est dans le plan (x,y) et orthogonal a uvw
-  x = (/sin(tab_RT_az(iaz) * deg_to_rad),-cos(tab_RT_az(iaz) * deg_to_rad),0._dp/)
+  x = (/cos(tab_RT_az(iaz) * deg_to_rad), sin(tab_RT_az(iaz) * deg_to_rad),0._dp/)
 
   ! Vecteur x image avec PA
   if (abs(ang_disque) > tiny_real) then
@@ -1185,12 +1185,11 @@ subroutine dust_map(lambda,ibin,iaz)
   endif
 
   ! Vecteur y image avec PA : orthogonal a x_plan_image et uvw
-  y_plan_image = cross_product(x_plan_image, uvw)
+  y_plan_image = -cross_product(x_plan_image, uvw)
 
   ! position initiale hors modele (du cote de l'observateur)
   ! = centre de l'image
   l = 10.*Rmax  ! on se met loin
-
 
   x0 = u * l  ;  y0 = v * l  ;  z0 = w * l
   center(1) = x0 ; center(2) = y0 ; center(3) = z0
@@ -1260,12 +1259,12 @@ subroutine dust_map(lambda,ibin,iaz)
   else ! method 2 : echantillonnage lineaire avec sous-pixels
 
      ! Vecteurs definissant les pixels (dx,dy) dans le repere universel
-     taille_pix = (map_size/ zoom) / real(max(igridx,igridy),kind=dp) ! en AU
+     taille_pix = (map_size/zoom) / real(max(igridx,igridy),kind=dp) ! en AU
      dx(:) = x_plan_image * taille_pix
      dy(:) = y_plan_image * taille_pix
 
      ! Coin en bas gauche de l'image
-     Icorner(:) = center(:) - ( 0.5* igridx * dx(:) +  0.5* igridy * dy(:))
+     Icorner(:) = center(:) - ( 0.5 * igridx * dx(:) +  0.5 * igridy * dy(:))
 
      if (l_sym_ima) then
         igridx_max = igridx/2 + modulo(igridx,2)
@@ -1305,7 +1304,7 @@ subroutine dust_map(lambda,ibin,iaz)
   endif ! method
 
   ! Adding stellar contribution
-  call compute_stars_map(lambda, iaz, u, v, w)
+  call compute_stars_map(lambda, u,v,w, dx,dy)
 
   id = 1
   Stokes_ray_tracing(lambda,:,:,ibin,iaz,1,id) = Stokes_ray_tracing(lambda,:,:,ibin,iaz,1,id) + stars_map(:,:,1)
@@ -1326,19 +1325,20 @@ end subroutine dust_map
 
 !***********************************************************
 
-subroutine compute_stars_map(lambda,iaz, u,v,w)
+subroutine compute_stars_map(lambda, u,v,w, dx_map, dy_map)
   ! Make a ray-traced map of the stars
 
   use utils, only : interp
 
-  integer, intent(in) :: lambda, iaz
+  integer, intent(in) :: lambda
   real(kind=dp), intent(in) :: u,v,w
+   real(kind=dp), dimension(3), intent(in) :: dx_map, dy_map
 
   integer, parameter :: n_ray_star_SED = 1024
 
   real(kind=dp), dimension(4) :: Stokes
-  real(kind=dp) :: facteur, facteur2, x0,y0,z0, x1, y1, lmin, lmax, norme, x, y, z, argmt, srw02
-  real :: cos_thet, cos_RT_az, sin_RT_az, rand, rand2, tau, pix_size, LimbDarkening, Pola_LimbDarkening, P, phi
+  real(kind=dp) :: facteur, facteur2, lmin, lmax, norme, x, y, z, argmt, srw02
+  real :: cos_thet, rand, rand2, tau, pix_size, LimbDarkening, Pola_LimbDarkening, P, phi
   integer, dimension(n_etoiles) :: n_ray_star
   integer :: id, icell, iray, istar, i,j, x_center, y_center, alloc_status
   logical :: in_map, lpola
@@ -1369,9 +1369,6 @@ subroutine compute_stars_map(lambda,iaz, u,v,w)
 
   x_center = npix_x/2 + 1
   y_center = npix_y/2 + 1
-
-  cos_RT_az = cos(tab_RT_az(iaz) * deg_to_rad)
-  sin_RT_az = sin(tab_RT_az(iaz) * deg_to_rad)
 
   ! Energie
   facteur = E_stars(lambda) * tab_lambda(lambda) * 1.0e-6 &
@@ -1411,8 +1408,8 @@ subroutine compute_stars_map(lambda,iaz, u,v,w)
      !$omp default(none) &
      !$omp shared(stream,istar,n_ray_star,llimb_darkening,limb_darkening,mu_limb_darkening,lsepar_pola) &
      !$omp shared(pola_limb_darkening,lambda,u,v,w,tab_RT_az,lsed,etoile,l3D,RT_sed_method,lpola) &
-     !$omp shared(x_center,y_center,nb_proc,map_1star,Q_1star,U_1star,cos_RT_az,sin_RT_az) &
-     !$omp private(id,i,j,iray,rand,rand2,x,y,z,srw02,argmt,cos_thet,LimbDarkening,x0,y0,z0,x1,y1,Stokes) &
+     !$omp shared(x_center,y_center,dx_map,dy_map,nb_proc,map_1star,Q_1star,U_1star) &
+     !$omp private(id,i,j,iray,rand,rand2,x,y,z,srw02,argmt,cos_thet,LimbDarkening,Stokes) &
      !$omp private(Pola_LimbDarkening,icell,tau,lmin,lmax,in_map,P,phi) &
      !$omp reduction(+:norme)
      in_map = .true. ! for SED
@@ -1442,29 +1439,22 @@ subroutine compute_stars_map(lambda,iaz, u,v,w)
               Pola_LimbDarkening = interp(pola_limb_darkening, mu_limb_darkening, cos_thet)
            endif
         endif
+
         ! Position de depart aleatoire sur une sphere de rayon r_etoile
-        x1 = etoile(istar)%x + x * etoile(istar)%r
-        y1 = etoile(istar)%y + y * etoile(istar)%r
-        z0 = etoile(istar)%z + z * etoile(istar)%r
+        x = etoile(istar)%x + x * etoile(istar)%r
+        y = etoile(istar)%y + y * etoile(istar)%r
+        z = etoile(istar)%z + z * etoile(istar)%r
 
         icell = etoile(istar)%icell
 
-        if (abs(w -1) < tiny_real) then ! rotating the position as the old "rotation" routine does not deal properly with case w==1
-           x0 = x1 * cos_RT_az + y1 * sin_RT_az
-           y0 = x1 * sin_RT_az - y1 * cos_RT_az
-        else ! the rotation information is already incoded in u,v,w
-           x0 = -x1
-           y0 = y1
-        endif
-
         Stokes = 0.0_dp
-        call optical_length_tot(1,lambda,Stokes,icell,x0,y0,z0,u,v,w,tau,lmin,lmax)
+        call optical_length_tot(1,lambda,Stokes,icell,x,y,z,u,v,w,tau,lmin,lmax)
 
         ! Coordonnees pixel
          if (lsed.and.(RT_sed_method == 1)) then
            i=1 ; j=1
         else
-           call find_pixel(x0,y0,z0, u,v,w, i,j,in_map)
+           call find_pixel(x,y,z, dx_map, dy_map, i,j,in_map)
         endif
 
         if (in_map) then
@@ -1502,6 +1492,36 @@ subroutine compute_stars_map(lambda,iaz, u,v,w)
   return
 
 end subroutine compute_stars_map
+
+!***********************************************************
+
+subroutine find_pixel(x,y,z,dx_map,dy_map, i, j, in_map)
+
+  real(kind=dp), intent(in) :: x,y,z
+  real(kind=dp), dimension(3), intent(in) :: dx_map, dy_map
+  integer, intent(out) :: i,j
+  logical, intent(out) :: in_map
+
+  real(kind=dp), dimension(3) :: xyz
+  real(kind=dp) :: x_map, y_map
+
+  xyz(1) = x ; xyz(2) = y ; xyz(3) = z
+
+  x_map = dot_product(xyz, dx_map)
+  y_map = dot_product(xyz, dy_map)
+
+  i = int(x_map) + npix_x/2 + 1
+  j = int(y_map) + npix_y/2 + 1
+
+  if ((i<1).or.(i>igridx).or.(j<1).or.(j>igridy)) then
+     in_map = .false.
+  else
+     in_map = .true.
+  endif
+
+  return
+
+end subroutine find_pixel
 
 !***********************************************************
 

src/mol_transfer.f90
@@ -562,10 +562,8 @@ subroutine emission_line_map(imol,ibin,iaz)
 
   ! Definition des vecteurs de base du plan image dans le repere universel
 
-  ! Definition des vecteurs de base du plan image dans le repere universel
-
   ! Vecteur x image sans PA : il est dans le plan (x,y) et orthogonal a uvw
-  x = (/sin(tab_RT_az(iaz) * deg_to_rad),-cos(tab_RT_az(iaz) * deg_to_rad),0._dp/)
+  x = (/cos(tab_RT_az(iaz) * deg_to_rad),sin(tab_RT_az(iaz) * deg_to_rad),0._dp/)
 
   ! Vecteur x image avec PA
   if (abs(ang_disque) > tiny_real) then
@@ -576,7 +574,7 @@ subroutine emission_line_map(imol,ibin,iaz)
   endif
 
   ! Vecteur y image avec PA : orthogonal a x_plan_image et uvw
-  y_plan_image = cross_product(x_plan_image, uvw)
+  y_plan_image = -cross_product(x_plan_image, uvw)
 
   ! position initiale hors modele (du cote de l'observateur)
   ! = centre de l'image
@@ -588,7 +586,6 @@ subroutine emission_line_map(imol,ibin,iaz)
   ! Coin en bas gauche de l'image
   Icorner(:) = center(:) - 0.5 * map_size * (x_plan_image + y_plan_image)
 
-
   if (RT_line_method == 1) then ! method 1 : echantillonanage log
      ! Pas de sous-pixel car les pixels ne sont pas carres
      n_iter_min = 1
@@ -599,7 +596,7 @@ subroutine emission_line_map(imol,ibin,iaz)
      i = 1
      j = 1
 
-     rmin_RT = max(w*0.9_dp,0.05_dp) * rmin
+     rmin_RT = max(w*0.9_dp,0.05_dp) * Rmin
      rmax_RT = 2.0_dp * Rmax
 
      tab_r(1) = rmin_RT
@@ -647,7 +644,7 @@ subroutine emission_line_map(imol,ibin,iaz)
   else ! method 2 : echantillonnage lineaire avec sous-pixels
 
      ! Vecteurs definissant les pixels (dx,dy) dans le repere universel
-     taille_pix = map_size / real(max(igridx,igridy),kind=dp) ! en AU
+     taille_pix = (map_size/zoom) / real(max(igridx,igridy),kind=dp) ! en AU
      dx(:) = x_plan_image * taille_pix
      dy(:) = y_plan_image * taille_pix
 
@@ -688,7 +685,7 @@ subroutine emission_line_map(imol,ibin,iaz)
   ! --------------------------
   do i = 1, mol(imol)%nTrans_raytracing
      lambda =  mol(imol)%indice_Trans_raytracing(i) ! == iTrans
-     call compute_stars_map(lambda, iaz, u, v, w)
+     call compute_stars_map(lambda, u, v, w, dx, dy)
 
      do iv =  -n_speed_rt, n_speed_rt
         spectre(:,:,iv,i,ibin,iaz) = spectre(:,:,iv,i,ibin,iaz) + stars_map(:,:,1)

src/output.f90
@@ -307,42 +307,6 @@ end subroutine capteur
 
 !**********************************************************************
 
-subroutine find_pixel(x,y,z,u,v,w, i, j, in_map)
-
-  real(kind=dp), intent(in) :: x,y,z,u,v,w
-  integer, intent(out) :: i,j
-  logical, intent(out) :: in_map
-
-  real(kind=dp) :: x2,y2,z2, y_map,z_map
-
-  !*****************************************************
-  !*----DETERMINATION DE LA POSITION SUR LA CARTE
-  !*----IL FAUT FAIRE UNE ROTATION DU POINT
-  !*    (X1,Y1,Z1) POUR RAMENER LES COORDONNEES DANS
-  !*    LE SYSTEME OU (U1,V1,W1)=(1,0,0)
-  !*****************************************************
-
-  call rotation(x,y,z, -u,v,w, x2,y2,z2)
-
-  ! rotation eventuelle du disque
-  y_map = y2 * cos_disk + z2 * sin_disk
-  z_map = z2 * cos_disk - y2 * sin_disk
-
-  i = int((y_map*zoom + 0.5*map_size)*size_pix) + deltapix_x
-  j = int((z_map*zoom + 0.5*map_size)*size_pix) + deltapix_y
-
-  if ((i<1).or.(i>igridx).or.(j<1).or.(j>igridy)) then
-     in_map = .false.
-  else
-     in_map = .true.
-  endif
-
-  return
-
-end subroutine find_pixel
-
-!**********************************************************************
-
 subroutine write_stokes_fits()
 
   implicit none

commit dfa6fe0982fdd7a60c5b430e6b7be4e772b1592b

Bug fix: correcting azimuthal reference in rt2 to match new map orientation The azimuthal angle is now defined relative to the -y axis and x anymore. Tested ok for rt1 and rt2. For rt2, tested ok for intensity and polarisation, for stellar component (ie scattered once) and multiple scattering component. Matches the results of v3.0.15. Bug was introduced in f966c3f8ecf02db51fb1de07c6369bf76dbceafa

src/dust_ray_tracing.f90
@@ -57,7 +57,7 @@ subroutine alloc_ray_tracing()
      n_theta_rt = 2
   endif
 
-  ! n_phi_I and n_theta_I are the angular diemensions of the store radiation field
+  ! n_phi_I and n_theta_I are the angular dimensions of the stored radiation field
   ! 15 15 90 90 OK pour benchmark
   n_phi_I = 15 ; ! 17/11/10 semble mieux si egal a nang_ray_tracing
   n_theta_I = 15 ;
@@ -253,6 +253,8 @@ subroutine init_directions_ray_tracing()
      ! pour les differentes directions de ray-tracing utilisees dans le RT2
      tab_uv_rt(ibin) = sin( sign(max(abs(tab_RT_incl(ibin)),1e-20), tab_RT_incl(ibin)) *deg_to_rad ) ! uv_rt mais v_rt = 0 ici
      tab_w_rt(ibin) = sqrt(1.0_dp - tab_uv_rt(ibin)*tab_uv_rt(ibin))
+
+     ! phi = 0 correspond a -v
      do iaz =1, RT_n_az
         tab_u_rt(ibin,iaz) =   tab_uv_rt(ibin) * sin(tab_RT_az(iaz)*deg_to_rad)
         tab_v_rt(ibin,iaz) =   - tab_uv_rt(ibin) * cos(tab_RT_az(iaz)*deg_to_rad)
@@ -287,9 +289,9 @@ subroutine angles_scatt_rt2(id,ibin,x,y,z,u,v,w,lstar)
   uv0 = tab_uv_rt(ibin) ; w0 = tab_w_rt(ibin) ! epsilon needed here
 
   ! Calcul angle phi a la position
-  phi_pos = modulo(atan2(y,x) + deux_pi,deux_pi)
+  phi_pos = modulo(atan2(x,y) + deux_pi,deux_pi)
 
-  ! les directions de ray-tracing sont definis pr rapport a la ref u0
+  ! Nouvelle orientation: les directions de ray-tracing sont definies par rapport a l'axe de ref y
   ! il faut se replacer dans ce cas en fct de la position
   ! ie il faut soustraire le phi de la position du paquet
 
@@ -307,10 +309,10 @@ subroutine angles_scatt_rt2(id,ibin,x,y,z,u,v,w,lstar)
         phi = deux_pi * real(iscatt) / real(N)
     ! endif
 
-     phi = phi + phi_pos
+     phi = phi - phi_pos
 
-     u_ray_tracing = uv0 * cos(phi)
-     v_ray_tracing = uv0 * sin(phi)
+     u_ray_tracing = uv0 * sin(phi)
+     v_ray_tracing = - uv0 * cos(phi)
      w_ray_tracing = w0
 
      prod1 = u_ray_tracing * u  + v_ray_tracing * v
@@ -880,8 +882,8 @@ subroutine calc_Isca_rt2(lambda,p_lambda,ibin)
   endif
 
   ! Position : virtuelle !!
-  x = 1.0_dp ! doit juste etre non nul
-  y = 0.0_dp
+  x = 0.0_dp
+  y = 1.0_dp ! doit juste etre non nul
   z = 1.0_dp ! sert a rien
 
   ! Champ de radiation
@@ -901,8 +903,8 @@ subroutine calc_Isca_rt2(lambda,p_lambda,ibin)
   do dir=0,1
      do iscatt = 1, nang_ray_tracing
         phi_scatt = deux_pi * real(iscatt) / real(nang_ray_tracing)  ! todo : precalculate this section
-        u_ray_tracing = uv0 * cos(phi_scatt)
-        v_ray_tracing = uv0 * sin(phi_scatt)
+        u_ray_tracing = uv0 * sin(phi_scatt)
+        v_ray_tracing = - uv0 * cos(phi_scatt)
         w_ray_tracing = w0 * (2 * dir - 1)
 
         do theta_I=1,n_theta_I
@@ -919,8 +921,8 @@ subroutine calc_Isca_rt2(lambda,p_lambda,ibin)
                     phi = deux_pi * (real(phi_I,kind=dp) - f2) / real(n_phi_I,kind=dp)
 
                     w02 = sqrt(1.0_dp-w*w)
-                    u = w02 * cos(phi)
-                    v = w02 * sin(phi)
+                    u = w02 * sin(phi)
+                    v = - w02 * cos(phi)
                     ! BUG : u_ray_tracing
 
                     ! Angle de diffusion --> n'utilise plus cos_thet_ray_tracing depuis super-echantillonage
@@ -949,8 +951,8 @@ subroutine calc_Isca_rt2(lambda,p_lambda,ibin)
                  phi = deux_pi * (real(phi_I,kind=dp) - 0.5) / real(n_phi_I,kind=dp)
 
                  w02 = sqrt(1.0_dp-w*w)
-                 u = w02 * cos(phi)
-                 v = w02 * sin(phi)
+                 u = w02 * sin(phi)
+                 v = - w02 * cos(phi)
 
                  ! Angle de diffusion
                  cos_scatt = u_ray_tracing * u + v_ray_tracing * v + w_ray_tracing * w
@@ -1022,7 +1024,6 @@ subroutine calc_Isca_rt2(lambda,p_lambda,ibin)
   RPO(4,4) = 1.0_dp
   ROP(4,4) = 1.0_dp
 
-
   if (.not.lvariable_dust) then   ! we precalculate the s11 as they are all the same
      icell = icell_ref
 
@@ -1061,8 +1062,8 @@ subroutine calc_Isca_rt2(lambda,p_lambda,ibin)
         do iscatt = 1, nang_ray_tracing
 
            phi_scatt = deux_pi * real(iscatt) / real(nang_ray_tracing)  ! todo : precalculate this section
-           u_ray_tracing = uv0 * cos(phi_scatt)
-           v_ray_tracing = uv0 * sin(phi_scatt)
+           u_ray_tracing = uv0 * sin(phi_scatt)
+           v_ray_tracing = - uv0 * cos(phi_scatt)
            w_ray_tracing = w0
 
            do theta_I=1,n_theta_I
@@ -1238,13 +1239,13 @@ subroutine calc_Isca_rt2_star(lambda,p_lambda,ibin)
      if (stokes(1) < 1.e-30_dp) cycle
 
      ! Direction de vol
-     x = r_grid(icell) ! doit juste etre non nul
-     y = 0.0_dp
+     x = 0.0_dp
+     y = r_grid(icell) ! doit juste etre non nul
      z = z_grid(icell) ! sert a rien
 
-     norme = sqrt(x**2 + z**2)
-     u = x / norme
-     v = 0.0_dp
+     norme = sqrt(y**2 + z**2)
+     u = 0.0_dp
+     v = y / norme
      w = z / norme
 
      ! cos_scatt_ray_tracing correspondants
@@ -1401,7 +1402,7 @@ function dust_source_fct(icell, x,y,z)
            psup=2
         endif
         ! Cette methode est OK mais on voit les cellules
-        phi_pos = atan2(y,x)
+        phi_pos = atan2(x,y)
         k = floor(modulo(phi_pos, deux_pi) / deux_pi * n_az_rt) + 1
         if (k > n_az_rt) k = n_az_rt
      endif
@@ -1477,7 +1478,7 @@ function dust_source_fct(icell, x,y,z)
 
 
      ! Calcul angle phi a la position
-     phi_pos = modulo(atan2(y,x) + deux_pi,deux_pi)
+     phi_pos = modulo(atan2(x,y) + deux_pi,deux_pi)
 
      if (z > 0.0_dp) then
         dir=1
@@ -1488,7 +1489,7 @@ function dust_source_fct(icell, x,y,z)
      !----------------------------------------------------
      ! Emissivite du champ diffuse plusieurs fois
      !----------------------------------------------------
-     xiscatt =   max(phi_pos/deux_pi  * nang_ray_tracing, 0.0_dp)
+     xiscatt = max(phi_pos/deux_pi  * nang_ray_tracing, 0.0_dp)
 
      iscatt1 = floor(xiscatt)
      frac = xiscatt - iscatt1
@@ -1517,7 +1518,7 @@ function dust_source_fct(icell, x,y,z)
      !----------------------------------------------------
      ! Emissivite du champ stellaire diffuse 1 fois
      !----------------------------------------------------
-     xiscatt =   max(phi_pos/deux_pi  * nang_ray_tracing_star, 0.0_dp)
+     xiscatt =  max(phi_pos/deux_pi  * nang_ray_tracing_star, 0.0_dp)
 
      iscatt1 = floor(xiscatt)
      frac = xiscatt - iscatt1

src/modules.f90
@@ -7,7 +7,7 @@ module parametres
   save
 
   real, parameter :: mcfost_version = 3.0
-  character(8), parameter :: mcfost_release = "3.0.16"
+  character(8), parameter :: mcfost_release = "3.0.17"
   real, parameter :: required_utils_version = 3.0
 
   character(len=128), parameter :: webpage=      "http://ipag.osug.fr/public/pintec/mcfost/"

src/optical_depth.f90
@@ -192,7 +192,7 @@ subroutine save_radiation_field(id,lambda,p_lambda,icell0, Stokes, l,  x0,y0,z0,
            phi_k = 1
            psup = 1
         else
-           phi_pos = atan2(ym,xm)
+           phi_pos = atan2(xm,ym)
            phi_k = floor(  modulo(phi_pos, deux_pi) / deux_pi * n_az_rt ) + 1
            if (phi_k > n_az_rt) phi_k=n_az_rt
 
@@ -218,9 +218,9 @@ subroutine save_radiation_field(id,lambda,p_lambda,icell0, Stokes, l,  x0,y0,z0,
            xm = 0.5_dp * (x0 + x1)
            ym = 0.5_dp * (y0 + y1)
            zm = 0.5_dp * (z0 + z1)
-           phi_pos = atan2(ym,xm)
+           phi_pos = atan2(xm,ym)
 
-           phi_vol = atan2(v,u) + deux_pi ! deux_pi pour assurer diff avec phi_pos > 0
+           phi_vol = atan2(-u,-v) + deux_pi ! deux_pi pour assurer diff avec phi_pos > 0
 
 
            !  if (l_sym_ima) then
@@ -229,7 +229,7 @@ subroutine save_radiation_field(id,lambda,p_lambda,icell0, Stokes, l,  x0,y0,z0,
            !     phi_I =  nint( delta_phi  / pi * (n_phi_I -1) ) + 1
            !     if (phi_I > n_phi_I) phi_I = n_phi_I
            !  else
-           phi_I =  floor(  modulo(phi_vol - phi_pos, deux_pi) / deux_pi * n_phi_I ) + 1
+           phi_I =  floor( modulo(phi_vol - phi_pos, deux_pi) / deux_pi * n_phi_I ) + 1
            if (phi_I > n_phi_I) phi_I = 1
            !  endif
 