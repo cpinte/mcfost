cpinte/mcfost

commit 4b0520be89df33cf4eda6c8867db20fbbb0e41cd

New interpolation method for polarised scattered specific intensity

src/dust_ray_tracing.f90
@@ -57,7 +57,7 @@ subroutine alloc_ray_tracing()
      n_theta_rt = 2
   endif
 
-  ! n_phi_I and n_theta_I are the angular dimensions of the stored radiation field
+  ! n_phi_I and n_theta_I are the angular dimensions of the stored radiation field for rt2
   ! 15 15 90 90 OK pour benchmark
   n_phi_I = 15 ; ! 17/11/10 semble mieux si egal a nang_ray_tracing
   n_theta_I = 15 ;
@@ -303,11 +303,11 @@ subroutine angles_scatt_rt2(id,ibin,x,y,z,u,v,w,lstar)
 
   ! creation des tableaux de u,v,w
   do iscatt=1, N
-    ! if (l_sym_ima) then
-    !    phi = pi * real(iscatt-1) / real(nang_ray_tracing-1) + pi
-    ! else
-        phi = deux_pi * real(iscatt) / real(N)
-    ! endif
+     ! if (l_sym_ima) then
+     !    phi = pi * real(iscatt-1) / real(nang_ray_tracing-1) + pi
+     ! else
+     phi = deux_pi * real(iscatt) / real(N)
+     ! endif
 
      phi = phi - phi_pos
 
@@ -1507,13 +1507,33 @@ function dust_source_fct(icell, x,y,z)
 
      ! Fct source des cellules
      icell_tmp = cell_map(ri1,zj1,phik)
-     SF1(:) = eps_dust2(:,iscatt2,dir,icell_tmp) * frac + eps_dust2(:,iscatt1,dir,icell_tmp) * un_m_frac
+     SF1(1) = eps_dust2(1,iscatt2,dir,icell_tmp) * frac + eps_dust2(1,iscatt1,dir,icell_tmp) * un_m_frac
+     if (lsepar_pola) then
+        SF1(2:3) = interpolate_Stokes_QU(eps_dust2(2:3,iscatt1,dir,icell_tmp), &
+             eps_dust2(2:3,iscatt2,dir,icell_tmp),un_m_frac)
+     endif
+
      icell_tmp = cell_map(ri2,zj1,phik)
-     SF2(:) = eps_dust2(:,iscatt2,dir,icell_tmp) * frac + eps_dust2(:,iscatt1,dir,icell_tmp) * un_m_frac
+     SF2(1) = eps_dust2(1,iscatt2,dir,icell_tmp) * frac + eps_dust2(1,iscatt1,dir,icell_tmp) * un_m_frac
+     if (lsepar_pola) then
+        SF2(2:3) = interpolate_Stokes_QU(eps_dust2(2:3,iscatt1,dir,icell_tmp), &
+             eps_dust2(2:3,iscatt2,dir,icell_tmp),un_m_frac)
+     endif
+
      icell_tmp = cell_map(ri1,zj2,phik)
-     SF3(:) = eps_dust2(:,iscatt2,dir,icell_tmp) * frac + eps_dust2(:,iscatt1,dir,icell_tmp) * un_m_frac
+     SF3(1) = eps_dust2(1,iscatt2,dir,icell_tmp) * frac + eps_dust2(1,iscatt1,dir,icell_tmp) * un_m_frac
+     if (lsepar_pola) then
+        SF3(2:3) = interpolate_Stokes_QU(eps_dust2(2:3,iscatt1,dir,icell_tmp), &
+             eps_dust2(2:3,iscatt2,dir,icell_tmp),un_m_frac)
+     endif
+
      icell_tmp = cell_map(ri2,zj2,phik)
-     SF4(:) = eps_dust2(:,iscatt2,dir,icell_tmp) * frac + eps_dust2(:,iscatt1,dir,icell_tmp) * un_m_frac
+     SF4(1) = eps_dust2(1,iscatt2,dir,icell_tmp) * frac + eps_dust2(1,iscatt1,dir,icell_tmp) * un_m_frac
+     if (lsepar_pola) then
+        SF4(2:3) = interpolate_Stokes_QU(eps_dust2(2:3,iscatt1,dir,icell_tmp), &
+             eps_dust2(2:3,iscatt2,dir,icell_tmp),un_m_frac)
+     endif
+
 
      !----------------------------------------------------
      ! Emissivite du champ stellaire diffuse 1 fois
@@ -1588,5 +1608,41 @@ function dust_source_fct(icell, x,y,z)
 end function dust_source_fct
 
 !***********************************************************
+
+function interpolate_Stokes_QU(QU1,QU2,frac1)
+  ! This fonction interpolates Stokes Q and U
+  ! by a linear interpolation in polarisation intensity and angle
+  ! as interpolating Q and U directly creates artefacts
+  ! (reduced pola when interpolating 2 Stokes vectors that are not aligned)
+  ! C. Pinte 1/11/2017
+
+  real, dimension(2), intent(in) :: QU1,QU2
+  real(dp), intent(in) :: frac1
+  real, dimension(2) :: interpolate_Stokes_QU
+
+  real :: PxI1, PxI2, PxI, deux_theta1, deux_theta2, deux_theta
+
+
+  PxI1 = sqrt(QU1(1)**2 + QU1(2)**2) ; deux_theta1 = atan2(QU1(2),QU1(1))
+  PxI2 = sqrt(QU2(1)**2 + QU2(2)**2) ; deux_theta2 = atan2(QU2(2),QU2(1))
+
+  ! Linear interpolation of polarized intensity
+  PxI = PxI2 * (1.0_dp-frac1) + PxI1 * frac1
+
+  ! Making sure there is no wrapping before the linear interpolation in angle
+  if (abs(deux_theta2 - deux_theta1) >= pi) then
+     if (deux_theta2 > deux_theta1) then
+        deux_theta1 = deux_theta1 + deux_pi
+     else
+        deux_theta2 = deux_theta2 + deux_pi
+     endif
+  endif
+  deux_theta = deux_theta2 * (1.0_dp-frac1) + deux_theta1 * frac1
+
+  interpolate_Stokes_QU = PxI * [cos(deux_theta),sin(deux_theta)]
+
+  return
+
+end function interpolate_Stokes_QU
 
 end module dust_ray_tracing

src/modules.f90
@@ -536,7 +536,7 @@ module constantes
   save
 
   ! Quelques reels utiles
-  real(kind=dp), parameter :: pi =3.141592653589793238462643383279502884197_dp ! ca devrait etre bon la
+  real(kind=dp), parameter :: pi = 3.141592653589793238462643383279502884197_dp ! ca devrait etre bon la
   real(kind=dp), parameter :: deux_pi = 2.0_dp * pi
   real(kind=dp), parameter :: un_sur_deux_pi = 1.0_dp/deux_pi
   real(kind=dp), parameter :: quatre_pi = 4.0_dp * pi

commit 572db0e52715eb2dda541a57069f3b3b80697b51

Bug fix: separation of contributions was broken since new pola interpolation
Bug was introduced in commits :
4b0520be89df33cf4eda6c8867db20fbbb0e41cd and
2cf140115680dfa2605d60eed9ff4ff28c2bb7ab

src/dust_ray_tracing.f90
@@ -1541,12 +1541,20 @@ function dust_source_fct(icell, x,y,z)
         SF1(2:3) = interpolate_Stokes_QU(eps_dust2(2:3,iscatt1,dir,icell_tmp), &
              eps_dust2(2:3,iscatt2,dir,icell_tmp),un_m_frac)
      endif
+     if (lsepar_contrib) then
+        SF1(n_pola+1:N_type_flux) = eps_dust2(n_pola+1:N_type_flux,iscatt2,dir,icell_tmp) * frac + &
+             eps_dust2(n_pola+1:N_type_flux,iscatt1,dir,icell_tmp) * un_m_frac
+     endif
 
      icell_tmp = cell_map(ri2,zj1,phik)
      SF2(1) = eps_dust2(1,iscatt2,dir,icell_tmp) * frac + eps_dust2(1,iscatt1,dir,icell_tmp) * un_m_frac
      if (lsepar_pola) then
         SF2(2:3) = interpolate_Stokes_QU(eps_dust2(2:3,iscatt1,dir,icell_tmp), &
              eps_dust2(2:3,iscatt2,dir,icell_tmp),un_m_frac)
+     endif
+     if (lsepar_contrib) then
+        SF2(n_pola+1:N_type_flux) = eps_dust2(n_pola+1:N_type_flux,iscatt2,dir,icell_tmp) * frac + &
+             eps_dust2(n_pola+1:N_type_flux,iscatt1,dir,icell_tmp) * un_m_frac
      endif
 
      icell_tmp = cell_map(ri1,zj2,phik)
@@ -1555,12 +1563,20 @@ function dust_source_fct(icell, x,y,z)
         SF3(2:3) = interpolate_Stokes_QU(eps_dust2(2:3,iscatt1,dir,icell_tmp), &
              eps_dust2(2:3,iscatt2,dir,icell_tmp),un_m_frac)
      endif
+     if (lsepar_contrib) then
+        SF3(n_pola+1:N_type_flux) = eps_dust2(n_pola+1:N_type_flux,iscatt2,dir,icell_tmp) * frac + &
+             eps_dust2(n_pola+1:N_type_flux,iscatt1,dir,icell_tmp) * un_m_frac
+     endif
 
      icell_tmp = cell_map(ri2,zj2,phik)
      SF4(1) = eps_dust2(1,iscatt2,dir,icell_tmp) * frac + eps_dust2(1,iscatt1,dir,icell_tmp) * un_m_frac
      if (lsepar_pola) then
         SF4(2:3) = interpolate_Stokes_QU(eps_dust2(2:3,iscatt1,dir,icell_tmp), &
              eps_dust2(2:3,iscatt2,dir,icell_tmp),un_m_frac)
+     endif
+     if (lsepar_contrib) then
+        SF4(n_pola+1:N_type_flux) = eps_dust2(n_pola+1:N_type_flux,iscatt2,dir,icell_tmp) * frac + &
+             eps_dust2(n_pola+1:N_type_flux,iscatt1,dir,icell_tmp) * un_m_frac
      endif
 
 
@@ -1589,9 +1605,13 @@ function dust_source_fct(icell, x,y,z)
      icell_tmp = cell_map(ri1,zj1,phik)
      SF1(1) = SF1(1) + eps_dust2_star(1,iscatt2,dir,icell_tmp) * frac &
           + eps_dust2_star(1,iscatt1,dir,icell_tmp) * un_m_frac
+
      if (lsepar_pola) then
+        n_pola = 4
         SF1(2:3) = SF1(2:3) + interpolate_Stokes_QU(eps_dust2_star(2:3,iscatt1,dir,icell_tmp), &
              eps_dust2_star(2:3,iscatt2,dir,icell_tmp),un_m_frac)
+     else
+        n_pola = 1
      endif
 
      icell_tmp = cell_map(ri2,zj1,phik)

src/modules.f90
@@ -7,7 +7,7 @@ module parametres
   save
 
   real, parameter :: mcfost_version = 3.0
-  character(8), parameter :: mcfost_release = "3.0.27"
+  character(8), parameter :: mcfost_release = "3.0.28"
   real, parameter :: required_utils_version = 3.0
 
   character(len=128), parameter :: webpage=      "http://ipag.osug.fr/public/pintec/mcfost/"