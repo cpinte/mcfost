9e77b4fe74bbfbc10f5f1b933faaae24588187ff
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2018-05-25 11:32:22 +1000

    Adding retro-compatibility with 2.11 parameter files (DENT grid)

--- src/read_param.f90
+++ src/read_param.f90
@@ -119,6 +119,12 @@ contains
           close(unit=1)
           call read_para212(para)
           return
+       else if (abs(para_version-2.11) < 1.e-4) then ! for the DENT grid
+          write(*,*) "Trying to read 2.11 parameter file."
+          write(*,*) "Pbs can appear. Parameter file should be updated !!!"
+          close(unit=1)
+          call read_para211(para)
+          return
        else
           close(unit=1)
           write(*,*) "Unsupported version of the parameter file :", para_version
@@ -3732,7 +3738,6 @@ end subroutine read_para215
     correct_Rsub = 1.0_dp
     dust_pop_tmp(:)%type = "Mie"
 
-
     ! -------------------------
     ! Number of photon packages
     ! -------------------------
@@ -4068,6 +4073,374 @@ end subroutine read_para215
     return
 
   end subroutine read_para212
+
+  !**********************************************************************
+
+  subroutine read_para211(para)
+
+    implicit none
+
+    character(len=*), intent(in) :: para
+
+    integer :: i, j, alloc_status, ios, tmpint, ind_pop, status, imol
+
+    real(kind=dp) :: size_neb_tmp, somme
+    real :: gas_dust
+
+    type(dust_pop_type), dimension(100) :: dust_pop_tmp
+    integer, dimension(100) :: n_especes
+
+    character(len=100) :: line_buffer
+
+    ! Lecture du fichier de parametres
+    open(unit=1, file=para, status='old')
+
+    read(1,*) para_version
+    correct_Rsub = 1.0_dp
+    dust_pop_tmp(:)%type = "Mie"
+    dust_pop_tmp(:)%mixing_rule=1
+
+    ! -------------------------
+    ! Number of photon packages
+    ! -------------------------
+    read(1,*) line_buffer
+    read(1,*) nbre_photons_loop ;  read(1,*) nbre_photons_eq_th ; read(1,*) nbre_photons_lambda ;
+    read(1,*) nbre_photons_image
+    nbre_photons_tot=real(nbre_photons_loop)*real(nbre_photons_eq_th)
+    tau_seuil  = 1.0e31
+    wl_seuil = 0.81
+
+    ! ----------
+    ! Wavelength
+    ! ----------
+    read(1,*) line_buffer
+    read(1,*) n_lambda, lambda_min, lambda_max
+    lmono0 = (n_lambda==1) ; lmono = lmono0
+    read(1,*) ltemp, lsed, lsed_complete
+    if (.not.lsed) lsed_complete = .false.
+    read(1,*) tab_wavelength
+    read(1,*) l_em_disk_image
+    read(1,*) lsepar_contrib, lsepar_pola
+
+    if (lsepar_pola) then
+       n_Stokes = 4
+       if (lsepar_contrib) then
+          N_type_flux = 8
+       else
+          N_type_flux = 4
+       endif
+    else
+       n_Stokes = 1
+       if (lsepar_contrib) then
+          N_type_flux = 5
+       else
+          N_type_flux = 1
+       endif
+    endif
+
+    ! -------------------------------
+    ! Grid geometry / input FITS file
+    ! -------------------------------
+    read(1,*) line_buffer
+    read(1,*) grid_type
+    read(1,*) n_rad, nz, n_az, n_rad_in
+    if ((.not.l3D).and.(n_az > 1)) then
+       write(*,*) "WARNING : n_az > 1 in 2D configuration, forcing n_az=1"
+       n_az=1
+    endif
+
+    ! ----
+    ! Maps
+    ! ----
+    read(1,*) line_buffer
+    read(1,*,iostat=ios) N_thet, N_phi, npix_x, npix_y, zoom
+    read(1,*) capt_interet, delta_capt, angle_interet, lonly_capt_interet
+
+    capt_inf=max(1,capt_interet-delta_capt)
+    capt_sup=min(N_thet,capt_interet+delta_capt)
+    if (lonly_capt_interet) then
+       N_incl = capt_sup - capt_inf + 1
+       capt_debut = capt_inf
+       capt_fin = capt_sup
+    else
+       N_incl = N_thet
+       capt_debut = 1
+       capt_fin = N_thet
+    endif
+    read(1,*) RT_imin, RT_imax, RT_n_incl, lRT_i_centered
+    read(1,*) distance
+    read(1,*) ang_disque
+
+    ! -----------------
+    ! Scattering method
+    ! -----------------
+    read(1,*) line_buffer
+    read(1,*) scattering_method
+    read(1,*) aniso_method
+
+    ! ----------
+    ! Symmetries
+    ! ----------
+    read(1,*) line_buffer
+    read(1,*) l_sym_ima
+    read(1,*) l_sym_centrale
+    read(1,*) l_sym_axiale
+
+
+    ! ----------------------
+    ! Dust global properties
+    ! ----------------------
+    read(1,*) line_buffer
+    read(1,*) gas_dust
+    read(1,*) lvariable_dust, exp_strat, a_strat
+    read(1,*,IOSTAT=status) ldust_sublimation, correct_Rsub
+    if (status/=0) correct_Rsub = 1.0
+    read(1,*) lchauff_int, alpha
+    read(1,*) T_min, T_max, n_T
+
+    ! ---------------
+    ! Number of zones
+    ! ---------------
+    read(1,*) line_buffer
+    read(1,*) n_zones
+    if (n_zones > 1) then
+       lvariable_dust=.true. ; exp_strat=0.
+       write(*,*) "You are using a n-zone parameter file"
+       write(*,*) "lstrat is set to true and exp_strat to 0."
+    endif
+    ! Allocation des variables pour disque a une zone
+    allocate(disk_zone(n_zones), stat=alloc_status)
+    if (alloc_status > 0) then
+       write(*,*) 'Allocation error disk parameters'
+       stop
+    endif
+    disk_zone%exp_beta=0.0; disk_zone%surf=0.0; disk_zone%sclht=0.0; disk_zone%diskmass=0.0; disk_zone%rref=0.0
+    disk_zone%rin=0.0 ; disk_zone%rout=0.0 ; disk_zone%edge=0.0
+
+    ! -----------------
+    ! Density structure
+    ! -----------------
+    read(1,*) line_buffer
+    do j=1,n_zones
+       read(1,*) disk_zone(j)%geometry
+       if (disk_zone(j)%geometry ==1) is_there_disk = .true.
+       if ((disk_zone(j)%geometry == 2).and.(grid_type == 1)) then
+          write(*,*) "WARNING : you are using an envelope density structure"
+          write(*,*) "          with a cylindrical grid !!!!"
+       endif
+       read(1,*) disk_zone(j)%diskmass
+       read(1,*) disk_zone(j)%sclht, disk_zone(j)%rref
+       read(1,*) disk_zone(j)%rin, disk_zone(j)%rout, size_neb_tmp, disk_zone(j)%edge
+           disk_zone(j)%Rmax = disk_zone(j)%Rout
+       read(1,*) disk_zone(j)%exp_beta
+       read(1,*) disk_zone(j)%surf
+
+       if (j==1) then
+          map_size=2*size_neb_tmp
+       else
+          if (abs(map_size-2*size_neb_tmp) > 1.e-6*map_size) then
+             write(*,*) "Error : different values for size_neb"
+             write(*,*) "Exiting"
+             stop
+          endif
+       endif
+    enddo ! n_zones
+
+    disk_zone(:)%gas_to_dust = gas_dust
+    disk_zone(:)%rmin = disk_zone(:)%rin - 5*disk_zone(:)%edge
+    rmin = minval(disk_zone(:)%rmin)
+    rmax = maxval(disk_zone(:)%rout)
+    diskmass = sum(disk_zone(:)%diskmass)
+
+    if (rmin < 0.0) then
+       write(*,*) "Error : r_min < 0.0"
+       stop
+    endif
+
+    ! ------
+    ! Cavity
+    ! ------
+    read(1,*) line_buffer
+    read(1,*) lcavity
+    read(1,*) cavity%sclht, cavity%rref
+    read(1,*) cavity%exp_beta
+
+    ! ----------------
+    ! Grain properties
+    ! ----------------
+    read(1,*) line_buffer
+    lRE_LTE=.false. ; lRE_nLTE=.false. ; lnRE=.false.
+    n_pop=0
+    do j=1, n_zones
+       read(1,*) n_especes(j)
+       somme=0.0
+       do i=1, n_especes(j)
+          n_pop = n_pop+1
+          read(1,*) dust_pop_tmp(n_pop)%indices(1), dust_pop_tmp(n_pop)%porosity, dust_pop_tmp(n_pop)%frac_mass
+          read(1,*) dust_pop_tmp(n_pop)%methode_chauffage
+          read(1,*) dust_pop_tmp(n_pop)%amin, dust_pop_tmp(n_pop)%amax, dust_pop_tmp(n_pop)%aexp, dust_pop_tmp(n_pop)%n_grains
+          if (dust_pop_tmp(n_pop)%methode_chauffage == 1) lRE_LTE=.true.
+          if (dust_pop_tmp(n_pop)%methode_chauffage == 2) lRE_nLTE=.true.
+          if (dust_pop_tmp(n_pop)%methode_chauffage == 3) lnRE=.true.
+          somme = somme + dust_pop_tmp(n_pop)%frac_mass
+          dust_pop_tmp(n_pop)%zone = j
+
+          dust_pop_tmp(n_pop)%n_components = 1
+          dust_pop_tmp(n_pop)%component_volume_fraction(1) =  1.0
+          dust_pop_tmp(n_pop)%is_PAH = .false.
+          dust_pop_tmp(n_pop)%is_opacity_file = .false.
+          dust_pop_tmp(n_pop)%is_Misselt_opacity_file = .false.
+          dust_pop_tmp(n_pop)%is_DustEM_opacity_file = .false.
+       enddo
+
+       ! renormalisation des fraction en masse
+       do i=1,n_especes(j)
+          dust_pop_tmp(n_pop-n_especes(j)+i)%frac_mass = dust_pop_tmp(n_pop-n_especes(j)+i)%frac_mass/somme
+          dust_pop_tmp(n_pop-n_especes(j)+i)%masse =  dust_pop_tmp(n_pop-n_especes(j)+i)%frac_mass * disk_zone(j)%diskmass
+       enddo
+
+    enddo !n_zones
+
+    if (lRE_LTE.and.lRE_nLTE) then
+       write(*,*) "Error : cannot mix grains in LTE and nLTE"
+       write(*,*) " Is it usefull anyway ???"
+       write(*,*) "Exiting"
+       stop
+    endif
+
+    ! variables triees
+    allocate(dust_pop(n_pop), stat=alloc_status)
+    if (alloc_status > 0) then
+       write(*,*) 'Allocation error n_pop tmp'
+       stop
+    endif
+    dust_pop%is_PAH = .false.
+
+    ! Classement des populations de grains : LTE puis nLTE puis nRE
+    ind_pop = 0
+    grain_RE_LTE_start = 1
+    grain_RE_LTE_end = 0
+    if (lRE_LTE) then
+       do i=1, n_pop
+          if (dust_pop_tmp(i)%methode_chauffage == 1) then
+             ind_pop=ind_pop+1
+             dust_pop(ind_pop) = dust_pop_tmp(i)
+             dust_pop(ind_pop)%ind_debut = grain_RE_LTE_end + 1
+             dust_pop(ind_pop)%ind_fin = grain_RE_LTE_end + dust_pop(ind_pop)%n_grains
+             grain_RE_LTE_end = grain_RE_LTE_end +  dust_pop(ind_pop)%n_grains
+
+          endif
+       enddo
+    endif
+
+    grain_RE_nLTE_start = grain_RE_LTE_end + 1
+    grain_RE_nLTE_end =  grain_RE_LTE_end
+    if (lRE_nLTE) then
+       do i=1, n_pop
+          if (dust_pop_tmp(i)%methode_chauffage == 2) then
+             ind_pop=ind_pop+1
+             dust_pop(ind_pop) = dust_pop_tmp(i)
+             dust_pop(ind_pop)%ind_debut = grain_RE_nLTE_end + 1
+             dust_pop(ind_pop)%ind_fin = grain_RE_nLTE_end + dust_pop(ind_pop)%n_grains
+             grain_RE_nLTE_end = grain_RE_nLTE_end +  dust_pop(ind_pop)%n_grains
+          endif
+       enddo
+    endif
+
+    grain_nRE_start = grain_RE_nLTE_end + 1
+    grain_nRE_end =  grain_RE_nLTE_end
+    if (lnRE) then
+       do i=1, n_pop
+          if (dust_pop_tmp(i)%methode_chauffage == 3) then
+             ind_pop=ind_pop+1
+             dust_pop(ind_pop) = dust_pop_tmp(i)
+             if (dust_pop(ind_pop)%indices(1)(1:3) == "PAH") then
+                dust_pop(ind_pop)%is_PAH = .true. ;dust_pop(ind_pop)%is_opacity_file = .true.
+                T_max = 2500.
+                if (lchange_Tmax_PAH) T_max = Tmax_PAH
+             endif
+             dust_pop(ind_pop)%ind_debut = grain_nRE_end + 1
+             dust_pop(ind_pop)%ind_fin = grain_nRE_end + dust_pop(ind_pop)%n_grains
+             grain_nRE_end = grain_nRE_end +  dust_pop(ind_pop)%n_grains
+          endif
+       enddo
+    endif
+
+    n_grains_RE_LTE = grain_RE_LTE_end  - grain_RE_LTE_start + 1
+    n_grains_RE_nLTE = grain_RE_nLTE_end  - grain_RE_nLTE_start + 1
+    n_grains_nRE = grain_nRE_end  - grain_nRE_start + 1
+
+    n_grains_tot=sum(dust_pop(:)%n_grains)
+
+    if (ldust_sublimation) then
+       if (n_lambda==1) then
+          write(*,*) "error : sub radius"
+       endif
+    endif
+
+    ! ---------------------
+    ! Molecular RT settings
+    ! ---------------------
+    read(1,*) line_buffer
+    read(1,*) lpop, lprecise_pop, lmol_LTE, largeur_profile
+    read(1,*) vitesse_turb
+    vitesse_turb = vitesse_turb * 1.e3 ! Conversion en m.s-1
+    read(1,*) n_molecules
+    allocate(mol(n_molecules))
+    do imol=1,n_molecules
+       read(1,*) mol(imol)%filename, mol(imol)%iLevel_max
+       read(1,*) mol(imol)%vmax_center_rt, mol(imol)%n_speed_rt
+       mol(imol)%vmax_center_rt = mol(imol)%vmax_center_rt * 1.e3 ! Conversion en m.s-1
+       read(1,*) mol(imol)%lcst_abundance, mol(imol)%abundance, mol(imol)%abundance_file
+       read(1,*) mol(imol)%lline, mol(imol)%nTrans_raytracing
+       read(1,*) mol(imol)%indice_Trans_rayTracing(1:mol(imol)%nTrans_raytracing)
+       mol(imol)%n_speed_center_rt = mol(imol)%n_speed_rt
+       mol(imol)%n_extraV_rt = 0 ; mol(imol)%extra_deltaV_rt = 0.0
+    enddo
+
+    ! ---------------
+    ! Star properties
+    ! ---------------
+    read(1,*) line_buffer
+    read(1,*,iostat=ios) n_etoiles
+    if (ios/=0) then
+       write(*,*) 'Error reading file: you are using a 2-zone disk parameter file'
+       write(*,*) 'You must use the [-2zone] option to calculate a 2-zone disk'
+       !   write(*,*) ' '
+       write(*,*) 'Exiting'
+       stop
+    endif
+    allocate(etoile(n_etoiles), stat=alloc_status)
+    if (alloc_status > 0) then
+       write(*,*) 'Allocation error etoile'
+       stop
+    endif
+
+    if (n_etoiles > 1) then
+       write(*,*) "Multiple illuminating stars! Cancelling all image symmetries"
+       l_sym_ima=.false.
+       l_sym_centrale=.false.
+       l_sym_axiale=.false.
+    endif
+
+    do i=1,n_etoiles
+       read(1,*) etoile(i)%T, etoile(i)%r, etoile(i)%M, etoile(i)%x, etoile(i)%y, etoile(i)%z, etoile(i)%lb_body, etoile(i)%fUV
+       if (.not.etoile(i)%lb_body) then
+          read(1,*) etoile(i)%spectre
+       else
+          read(1,*)
+       endif
+       ! Passage rayon en AU
+       etoile(i)%r = etoile(i)%r * Rsun_to_AU
+    enddo
+
+    close(unit=1)
+
+    nbre_photons_lim = nbre_photons_lim*real(N_thet)*real(N_phi)*real(nbre_photons_lambda)
+
+    return
+
+  end subroutine read_para211
 
   !**********************************************************************
 
