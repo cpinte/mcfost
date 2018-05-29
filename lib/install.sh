28d0d224ffb477034244ec039321d932d4c66b46
Clément Robert <clement.robert@oca.eu>
2018-05-29 21:15:07 +0200

    add conditional chaining of instructions, + more robust fallback for cd between steps

--- lib/install.sh
+++ lib/install.sh
@@ -42,31 +42,39 @@ else
     exit
 fi
 
+#clear previous files
 rm -rf lib include
 mkdir lib
 mkdir include
 
+ANCHOR=$(pwd)
+
+
+
 # SPRNG
 # wget http://sprng.cs.fsu.edu/Version2.0/sprng2.0b.tar.gz
-tar xzvf sprng2.0b.tar.gz
-\cp -f $SYSTEM/make.CHOICES sprng2.0
-\cp -f $SYSTEM/make.INTEL sprng2.0/SRC
-if [ $(uname | tr '[a-z]' '[A-Z]' 2>&1 | grep -c DARWIN) -eq 1 ]; then
+tar xzvf sprng2.0b.tar.gz \
+&& \cp -f $SYSTEM/make.CHOICES sprng2.0 \
+&& \cp -f $SYSTEM/make.INTEL sprng2.0/SRC \
+&& if [ $(uname | tr '[a-z]' '[A-Z]' 2>&1 | grep -c DARWIN) -eq 1 ]; then
   \cp -f macos/insertmenu.mac sprng2.0/SRC/insertmenu
 fi
-cd sprng2.0
-make -B
-\cp lib/libsprng.a ../lib
-\cp include/*.h ../include
-cd ..
+cd sprng2.0 \
+&& make -B \
+&& \cp lib/libsprng.a ../lib \
+&& \cp include/*.h ../include
+
+cd $ANCHOR
 rm -rf sprng2.0
+
+
 
 # cfitsio
 # g77 ou f77 needed by configure to set up the fortran wrapper in Makefile
 # wget ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3420.tar.gz
-tar xzvf cfitsio3420.tar.gz
-cd cfitsio
-if [ "$SYSTEM" = "ifort" ] ; then
+tar xzvf cfitsio3420.tar.gz \
+&& cd cfitsio \
+&& if [ "$SYSTEM" = "ifort" ] ; then
     export CC="icc"
     export FC="ifort"
 elif [ "$SYSTEM" = "gfortran" ] ; then
@@ -74,37 +82,44 @@ elif [ "$SYSTEM" = "gfortran" ] ; then
     export FC="gfortran"
 elif [ "$SYSTEM" = "xeon-phi" ] ; then
     export CFLAGS=-mmic
-fi
-./configure --enable-ssse3
-if [ "$SYSTEM" = "xeon-phi" ] ; then
+fi \
+&& ./configure --enable-ssse3 \
+&& if [ "$SYSTEM" = "xeon-phi" ] ; then
     \cp -f ../Makefile_cfitsio.xeon_phi Makefile
 fi
 # We do not want to have to link with libcurl
-cat Makefile | sed s/-DCFITSIO_HAVE_CURL=1// |  sed s/-lcurl// >> Makefile.tmp && \mv -f Makefile.tmp Makefile
-make
-\cp libcfitsio.a ../lib
-cd ..
+cat Makefile | sed s/-DCFITSIO_HAVE_CURL=1// | sed s/-lcurl// >> Makefile.tmp && \mv -f Makefile.tmp Makefile \
+&& make \
+&& \cp libcfitsio.a ../lib
+
+cd $ANCHOR
 rm -rf cfitsio
+
+
 
 # voro++
 # Original voro++ can be obtained from
 # svn checkout --username anonsvn https://code.lbl.gov/svn/voro/trunk voro
+
 # Downloading last version tested with mcfost :
-git clone git@bitbucket.org:cpinte/voro.git
-if [ "$SYSTEM" = "ifort" ] ; then
+git clone git@bitbucket.org:cpinte/voro.git \
+&& if [ "$SYSTEM" = "ifort" ] ; then
     \cp -f  ifort/config.mk voro
 elif [ "$SYSTEM" = "xeon-phi" ] ; then
     \cp -f  ifort/config.mk voro # To be tested
 elif [ "$SYSTEM" = "gfortran" ] ; then
     \cp -f  gfortran/config.mk voro
-fi
-cd voro
-make
-\cp src/libvoro++.a ../lib
-mkdir -p ../include/voro++
-\cp src/*.hh ../include/voro++/
-cd ..
+fi \
+&& cd voro \
+&& make \
+&& \cp src/libvoro++.a ../lib \
+&& mkdir -p ../include/voro++ \
+&& \cp src/*.hh ../include/voro++/ \
+
+cd $ANCHOR
 rm -rf voro
+
+
 
 # Put in final directory
 mkdir -p $MCFOST_INSTALL/include
