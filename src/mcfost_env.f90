05998b8310bc37c4214b3c31123012729a2bee9d
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2018-09-07 10:16:21 +1000

    Stokes vectors follow the IAU convention

--- src/mcfost_env.f90
+++ src/mcfost_env.f90
@@ -5,7 +5,7 @@ module mcfost_env
   implicit none
 
   real, parameter :: mcfost_version = 3.0
-  character(8), parameter :: mcfost_release = "3.0.31"
+  character(8), parameter :: mcfost_release = "3.0.32"
   real, parameter :: required_utils_version = 3.0
 
   character(len=128), parameter :: webpage       = "http://ipag.osug.fr/public/pintec/mcfost/"
