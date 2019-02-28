7e9950ce54815b6923617c07854b32381f35d6b9
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2019-02-28 15:01:10 +1100

    setting the condition for cutting cells as in master. Just like it was before modification for atom RT

--- src/Voronoi.f90
+++ src/Voronoi.f90
@@ -250,8 +250,7 @@ module Voronoi_grid
                    dy = y(i) - etoile(istar)%y
                    dz = z(i) - etoile(istar)%z
 
-                   !if (min(dx,dy,dz) < 2*etoile(istar)%r) then
-                   if ((min(dx,dy,dz) < 2*etoile(istar)%r).and.(h(i)<etoile(istar)%r)) then
+                   if (min(dx,dy,dz) < 2*etoile(istar)%r) then
                       dist2 = dx**2 + dy**2 + dz**2
                       if (dist2 < deuxr2_star(istar)) then
                          is_outside_stars = .false.
