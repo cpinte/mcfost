4bffee678c040d77c70ad6e4d90543d4df4c4a55
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2019-03-06 14:24:02 +1100

    Implementing NLTE. Radiative rates are scalar now, and CoolingRates_ij are stored on the whole grid

---
+++ src/Atoms/statequil_atoms.f90
@@ -0,0 +1,75 @@
+MODULE statequil_atoms
+
+ use atmos_type, only : atmos
+ use atom_type
+ use spectrum_type, only : NLTEspec
+
+ IMPLICIT NONE
+
+ CONTAINS
+ 
+
+ SUBROUTINE allocNetCoolingRates(atom)
+ ! ----------------------------------------------- !
+  ! For each transition of AtomType atom
+  ! allocate cooling rates.
+  ! It avoids to store 2 x Ntransitons * Nspace
+  ! arrays of radiative rates.
+  ! Instead Ntranstions * Nspace arrays of cooling
+  ! rates
+ ! ----------------------------------------------- !
+  integer :: kc, kr
+  type (AtomType), intent(inout) :: atom
+  
+   do kc=1,atom%Ncont
+    allocate(atom%continua(kc)%CoolRates_ij(atmos%Nspace))
+    atom%continua(kc)%CoolRates_ij(:) = 0d0
+   end do
+   do kr=1,atom%Nline
+    allocate(atom%lines(kr)%CoolRates_ij(atmos%Nspace))
+    atom%lines(kr)%CoolRates_ij(:) = 0d0
+   end do 
+
+ RETURN
+ END SUBROUTINE allocNetCoolingRates
+ 
+ SUBROUTINE initRadiativeRates(atom)
+ ! ---------------------------------------- !
+  ! For each transition of AtomType atom
+  ! set radiative rates to 0
+ ! ---------------------------------------- !
+  integer :: kc, kr
+  type (AtomType), intent(inout) :: atom
+  
+   do kc=1,atom%Ncont
+    atom%continua(kc)%Rij = 0d0
+    atom%continua(kc)%Rji = 0d0
+   end do
+   do kr=1,atom%Nline
+    atom%lines(kr)%Rij = 0d0
+    atom%lines(kr)%Rji = 0d0
+   end do 
+   
+ RETURN
+ END SUBROUTINE initRadiativeRates
+ 
+ 
+ SUBROUTINE addtoRadiativeRates()
+ ! ---------------------------------------- !
+  ! For each atoms and each transitions
+  ! compute the radiative rates
+  !
+  !
+ ! ---------------------------------------- !
+ 
+ 
+ RETURN
+ END SUBROUTINE addtoRadiativeRates
+
+ SUBROUTINE statequil()
+
+ RETURN
+ END SUBROUTINE statequil
+
+
+END MODULE statequil_atoms
