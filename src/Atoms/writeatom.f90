aa8efbbaa10f72eb92a47c2f1a871b7ed5da9709
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2019-06-20 10:08:39 +0200

    Modification of writeElectron. Now electron file is a .fits instead of .fits.gz. This allow to reopen the file to write the nlte electron density in case it is iterated within the NLTEloop

--- src/Atoms/writeatom.f90
+++ src/Atoms/writeatom.f90
@@ -23,7 +23,12 @@ MODULE writeatom !Futur write....
 
  IMPLICIT NONE 
  
- character(len=15), parameter :: neFile = "ne.fits.gz"
+ !Compressed files may only be opened with read-only permission
+ ! So if ne is iterated we re open ne.fits to write the nlte pops, so It cannot be
+ ! compressed.
+ !
+ !
+ character(len=15), parameter :: neFile = "ne.fits"
  character(len=15), parameter :: nHFile = "nHtot.fits.gz"
  character(len=50), parameter :: TemperatureFile = "Temperature.fits.gz"
 
@@ -41,54 +46,64 @@ MODULE writeatom !Futur write....
  RETURN
  END SUBROUTINE make_checkpoint
  
- SUBROUTINE writeElectron()
+ SUBROUTINE writeElectron(write_ne_nlte)
  ! ------------------------------------ !
  ! write Electron density if computed 
  ! by the code.
  ! The electron density, ne, is in m^-3
  ! ------------------------------------ !
   integer :: unit, EOF = 0, blocksize, naxes(4), naxis,group, bitpix, fpixel
-  logical :: extend, simple
-  integer :: nelements
+  logical :: extend, simple, already_exists = .false.
+  logical, optional :: write_ne_nlte
+  integer :: nelements, nfirst
   
+  if (present(write_ne_nlte)) already_exists = (write_ne_nlte==.true.) !append the file
   !get unique unit number
   CALL ftgiou(unit,EOF)
-
   blocksize=1
-  CALL ftinit(unit,trim(neFile),blocksize,EOF)
-  !  Initialize parameters about the FITS image
   simple = .true. !Standard fits
   group = 1 !??
   fpixel = 1
-  extend = .false. !??
+  extend = .true.
   bitpix = -64  
 
   if (lVoronoi) then
-   naxis = 1
-   naxes(1) = atmos%Nspace ! equivalent n_cells
-   nelements = naxes(1)
+   	naxis = 1
+   	naxes(1) = atmos%Nspace ! equivalent n_cells
+   	nelements = naxes(1)
   else
-   if (l3D) then
-    naxis = 3
-    naxes(1) = n_rad
-    naxes(2) = 2*nz
-    naxes(3) = n_az
-    nelements = naxes(1) * naxes(2) * naxes(3) ! != n_cells ? should be
-   else
-    naxis = 2
-    naxes(1) = n_rad
-    naxes(2) = nz
-    nelements = naxes(1) * naxes(2) ! should be n_cells also
-   end if
+   	if (l3D) then
+    	naxis = 3
+    	naxes(1) = n_rad
+   	 	naxes(2) = 2*nz
+    	naxes(3) = n_az
+    	nelements = naxes(1) * naxes(2) * naxes(3) ! != n_cells ? should be
+   	else
+    	naxis = 2
+    	naxes(1) = n_rad
+    	naxes(2) = nz
+    	nelements = naxes(1) * naxes(2) ! should be n_cells also
+   	end if
   end if
   ! write(*,*) 'yoyo2', n_rad, nz, n_cells, atmos%Nspace
   !  Write the required header keywords.
-
-  CALL ftphpr(unit,simple,bitpix,naxis,naxes,0,1,extend,EOF)
+  
+  if (already_exists) then !exits, expect to write nlte, read lte ne, append the file and write
+  	CALL ftopen(unit, TRIM(neFile), 1, blocksize, EOF) !1 stends for read and write
+  	!Compressed files may only be opened with read-only permission, so neFile is not .gz
+   	CALL ftcrhd(unit, EOF)
+   	CALL ftphpr(unit,simple,bitpix,naxis,naxes,0,1,extend,EOF)
+  	CALL ftpkys(unit, "UNIT", "m^-3", '(NLTE)', EOF)
+  	CALL ftpprd(unit,group,fpixel,nelements,atmos%ne,EOF)
+  else !not exist, expected not nlte, create and write
+  	CALL ftinit(unit,trim(neFile),blocksize,EOF)
+  !  Initialize parameters about the FITS image
+  	CALL ftphpr(unit,simple,bitpix,naxis,naxes,0,1,extend,EOF)
   ! Additional optional keywords
-  CALL ftpkys(unit, "UNIT", "m^-3", ' ', EOF)
+  	CALL ftpkys(unit, "UNIT", "m^-3", ' ', EOF)
   !write data
-  CALL ftpprd(unit,group,fpixel,nelements,atmos%ne,EOF)
+  	CALL ftpprd(unit,group,fpixel,nelements,atmos%ne,EOF)
+  end if !alreayd_exists
   
   CALL ftclos(unit, EOF)
   CALL ftfiou(unit, EOF)
