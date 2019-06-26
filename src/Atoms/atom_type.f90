a0cc000776a574baf9c1899df551942e6eab84d2
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2019-06-26 11:35:29 +0200

    Removing wlam array, which is now computed on the fly. Added a new wlam_norm array which is the sum(phi*dv) for a specific ray

--- src/Atoms/atom_type.f90
+++ src/Atoms/atom_type.f90
@@ -45,7 +45,7 @@ MODULE atom_type
    real(8), dimension(4) :: cvdWaals
    real(8), allocatable, dimension(:)  :: phi!, phi_Q, phi_U, phi_V, psi_Q, psi_U, psi_V
    !wlam is the integration wavelenght weigh = phi
-   double precision, allocatable, dimension(:)  :: lambda, CoolRates_ij, wlam, Jbar!, c_shift, c_fraction
+   double precision, allocatable, dimension(:)  :: lambda, CoolRates_ij, Jbar, wlam_norm!,wlam, c_shift, c_fraction
    double precision :: Qelast, adamp, Rij, Rji
    real(8), allocatable, dimension(:,:) :: rho_pfr
    !!Nlevel, wavelength and proc
@@ -63,7 +63,7 @@ MODULE atom_type
    logical :: hydrogenic
    integer :: i, j, Nlambda, Nblue = 0, Nred = 0, Nmid = 0
    real(8) :: lambda0, isotope_Frac, alpha0, lambdamin !continuum maximum frequency > frequency photoionisation
-   real(8), allocatable, dimension(:)  :: lambda, alpha, CoolRates_ij, Jbar, wlam
+   real(8), allocatable, dimension(:)  :: lambda, alpha, CoolRates_ij, Jbar!, wlam
    double precision :: Rji, Rij
    character(len=ATOM_LABEL_WIDTH) :: name !read in the atomic file
    type (AtomType), pointer :: atom => NULL()
