88fa08800837db02c41d29e3e93aae4e5dda980a
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2019-09-03 15:40:26 +0200

    Using faster H and He rayleigh scattering function

--- src/Atoms/rayleigh_scattering.f90
+++ src/Atoms/rayleigh_scattering.f90
@@ -29,16 +29,16 @@ MODULE rayleigh_scattering
   scatt = 0d0
 
   where(NLTEspec%lambda >= lambda_limit)
-   scatt = (1d0 + (1566d0/NLTEspec%lambda)**2.d0 + &
-   			(1484.d0/NLTEspec%lambda)**4d0)*(966.d0/NLTEspec%lambda)**4d0
+   scatt = (1d0 + (156.6d0/NLTEspec%lambda)**2.d0 + &
+   			(148.d0/NLTEspec%lambda)**4d0)*(96.6d0/NLTEspec%lambda)**4d0
   end where
 
   if (lstore_opac) then
    NLTEspec%AtomOpac%Kc(icell,:,1) = NLTEspec%AtomOpac%Kc(icell,:,1) + &
-   									 scatt * sigma_e * Hydrogen%n(icell,1) !m^-1
+   									 scatt * sigma_e * Hydrogen%n(1,icell)!atmos%nHtot(icell) !m^-1
   else
    NLTEspec%AtomOpac%sca_c(:,id) = NLTEspec%AtomOpac%sca_c(:,id) + &
-   									 scatt * sigma_e * Hydrogen%n(icell,1)
+   									 scatt * sigma_e * Hydrogen%n(1,icell)
   end if
    
  RETURN
@@ -57,23 +57,22 @@ MODULE rayleigh_scattering
   
   !if (atom%ID /= "He") RETURN
   if (.not.associated(Helium)) RETURN
-  write(*,*) "Computing HE I rayleigh"
   
   lambda_limit = 121.6d0 !which one for helium?
   scatt = 0d0
 
 
   where(NLTEspec%lambda >= lambda_limit)
-   scatt = 4d0 * (1d0 + (669d0/NLTEspec%lambda)**2.d0 + &
-   			(641d0/NLTEspec%lambda)**4d0)*(379d0/NLTEspec%lambda)**4d0
+   scatt = 4d0 * (1d0 + (66.9d0/NLTEspec%lambda)**2.d0 + &
+   			(64.1d0/NLTEspec%lambda)**4d0)*(37.9d0/NLTEspec%lambda)**4d0
   end where
 
   if (lstore_opac) then
    NLTEspec%AtomOpac%Kc(icell,:,1) = NLTEspec%AtomOpac%Kc(icell,:,1) + &
-   									 scatt * sigma_e * Helium%n(icell,1) !m^-1
+   									 scatt * sigma_e * Helium%n(1,icell) !m^-1
   else
    NLTEspec%AtomOpac%sca_c(:,id) = NLTEspec%AtomOpac%sca_c(:,id) + &
-   									 scatt * sigma_e * Helium%n(icell,1)
+   									 scatt * sigma_e * Helium%n(1,icell)
   end if
    
  RETURN
@@ -112,7 +111,7 @@ MODULE rayleigh_scattering
                 !of this atom in opac module.
                 
   scatt = 0d0
-  if (atom%ID=="He") write(*,*) "Rayleigh for Helium" !a test to remove later
+  !if (atom%ID=="He") write(*,*) "Rayleigh for Helium" !a test to remove later
 
   if (atom%stage(1) /= 0) then
    write(*,*) "Lowest level of atom is not a neutral stage"
@@ -122,14 +121,14 @@ MODULE rayleigh_scattering
   end if
   
   !!!!
-  !!!! There might be a bug here for image, because we do not do the sum over all transitions
-       ! since only contributing transitions are kept (Nred=Nblue=-99).  
+  !!!! CANNOT REMOVE TRANSITIONS EVEN IF THEY DO NO CONTRIBUTE TO OPAC. BECAUSE WE NEED TO SUM OVER ALL OF THEM
+  !!!! TO COMPUTE THE CROSS-SECTIONS
   !!!!
   
  !   find lambda_red, the longest wavelength covered by the
  !   transition less than lambda for this atom.
    do kr=1,atom%Nline
-    if (.not.atom%lines(kr)%lcontrib_to_opac) CYCLE
+    !!if (.not.atom%at(kr)%lcontrib_to_opac) CYCLE
     ! BEWARE: a line expands from Nblue to Nred but the reddest wavelength
     ! is line%lambda(Nlambda) = NLTEspec%lambda(Nred) by construction.
     !In getlambda.f90, a new grid is constructed and lambda(Nlambda) might not
@@ -144,7 +143,7 @@ MODULE rayleigh_scattering
   lambda2 = 0d0
 
  do kr=1,atom%Nline
-  if (.not.atom%lines(kr)%lcontrib_to_opac) CYCLE
+  !!if (.not.atom%at(kr)%lcontrib_to_opac) CYCLE
   lambda_red =  NLTEspec%lambda(atom%lines(kr)%Nred)
    if (atom%lines(kr)%i == 1) then
     where((NLTEspec%lambda > lambda_limit).and.(NLTEspec%lambda > lambda_red))
@@ -167,82 +166,5 @@ MODULE rayleigh_scattering
 
  RETURN
  END SUBROUTINE Rayleigh
-!  
-!  FUNCTION Rayleigh(icell, atom, scatt) result(res)
-!  ! ------------------------------------------------------------- !
-!   ! Rayleigh scattering by transitions from the ground state of
-!   ! neutral atoms.
-!   ! Sums scattering crosssections of all bound-bound
-!   ! transitions from the groundstate of the atom with lamda_red
-!   ! (the longest wavelength covered by the transition) less than wavelength 
-!   ! lambda.
-!   !
-!   ! See:
-!   ! -- Hubeny & Mihalas 2014 p. 155, eq. 6.44-6.45
-!   ! SigmaR \propto sigmaT * sum(f/(lambda/lambda0 - 1))**2
-!   !
-!   !
-!   ! Return scattering cross-section for all wavelengths
-!  ! ------------------------------------------------------------- !
-!   use math, only            								 : dpow
-!   type (AtomType), intent(in)                               :: atom
-!   integer, intent(in)                                       :: icell
-!   logical                                                   :: res
-!   double precision, dimension(NLTEspec%Nwaves), intent(out) :: scatt
-!   integer                                                   :: kr, k, la
-!   double precision                                          :: lambda_red, lambda_limit, &
-!    															   sigma_e
-!   double precision, dimension(NLTEspec%Nwaves) 				:: fomega, lambda2
-!   
-!   lambda_limit = LONG_RAYLEIGH_WAVE
-!   sigma_e = 8.0*PI/3.0 * &
-!          dpow(Q_ELECTRON/(dsqrt(4.0*PI*EPSILON_0) *&
-!          (dsqrt(M_ELECTRON)*CLIGHT)), 4.d0)
-!   res = .false. !init, if .false. do not consider Rayleigh
-!                 !of this atom in opac module.
-!                 
-!   scatt = 0.
-! 
-!   if (atom%stage(1) /= 0) then
-!    write(*,*) "Lowest level of atom is not a neutral stage"
-!    write(*,*) "Not computing rayleigh for this atom", atom%ID
-!    res = .false.
-!    RETURN
-!   end if
-!   
-!  !   find lambda_red, the longest wavelength covered by the
-!  !   transition less than lambda for this atom.
-!    do kr=1,atom%Nline
-!     ! BEWARE: a line expands from Nblue to Nred but the reddest wavelength
-!     ! is line%lambda(Nlambda) = NLTEspec%lambda(Nred) by construction.
-!     !In getlambda.f90, a new grid is constructed and lambda(Nlambda) might not
-!     !exist anymore..
-!     if (atom%lines(kr)%i == 1) then
-!      lambda_red = NLTEspec%lambda(atom%lines(kr)%Nred) !reddest wavelength including
-!     														!velocity shifts.
-!      lambda_limit = min(lambda_limit, lambda_red)
-!     end if
-!    end do
-!   fomega = 0.0
-!   lambda2 = 0d0
-! 
-!  do kr=1,atom%Nline
-!   lambda_red =  NLTEspec%lambda(atom%lines(kr)%Nred)
-!    if (atom%lines(kr)%i == 1) then
-!     where((NLTEspec%lambda > lambda_limit).and.(NLTEspec%lambda > lambda_red))
-!      lambda2 = 1./((NLTEspec%lambda/atom%lines(kr)%lambda0)**2 -1.)
-!      fomega = fomega + (lambda2)**2 *atom%lines(kr)%fosc
-!     end where
-!    end if 
-!  end do
-! 
-!   scatt = sigma_e * fomega * atom%n(1,icell) !m^-1 = m^2 * m^-3
-!   
-!   if ((MAXVAL(scatt).gt.0)) then
-!     res = .true.
-!     RETURN
-!   end if
-! 
-!  RETURN
-!  END FUNCTION Rayleigh
+
 END MODULE rayleigh_scattering
