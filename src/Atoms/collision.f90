8215e120f3ee9eccb2838bc187a3a11ef89b0a20
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2019-09-05 17:16:33 +0200

    future deprecation, use impact.f90 instead

--- src/Atoms/collision.f90
+++ src/Atoms/collision.f90
@@ -9,7 +9,7 @@
 !     ji = (j-1)*atom%Nlevel +i : i-->j
 ! ----------------------------------------------------------------------
 
-
+!! FUTURE DEPRECATION OF THE MODULE, IMPACT.f90 IS THE GOOD ONE
 MODULE collision
  use math, only : E1, E2, bezier3_interp, SQ, CUBE, dPOW, interp1D
  use constant
@@ -30,202 +30,202 @@ MODULE collision
 
  CONTAINS
  
- FUNCTION Collision_Hydrogen(icell) result(Cji)
-  integer :: icell, i, j
-  double precision :: Cji(Hydrogen%Nlevel, Hydrogen%Nlevel)
-  double precision :: nr_ji, CI(Hydrogen%Nlevel,Hydrogen%Nlevel), CE(Hydrogen%Nlevel,Hydrogen%Nlevel)
-  
-   Cji(:,:) = 0d0; CI = 0d0; CE(:,:) = 0d0 
-   CALL Johnson_CI(icell, CI(:,Hydrogen%Nlevel)) !bound-free i->Nlevel
-   CALL Johnson_CE(icell, CE) !among all levels
-
-   do j=1,Hydrogen%Nlevel
-    do i=1,Hydrogen%Nlevel
-     nr_ji = Hydrogen%nstar(i,icell)/Hydrogen%nstar(j,icell)
-     Cji(j,i) = CE(j,i) +  CI(i,j)  * nr_ji
-     Cji(i,j) = CE(j,i)/nr_ji + CI(i,j)
-    end do
-   end do
-   Cji(:,:) = Cji(:,:) * atmos%ne(icell)
-
- RETURN
- END FUNCTION Collision_Hydrogen
-
- SUBROUTINE Johnson_CI(icell, Cje)
- ! --------------------------------------------------- !
-  ! Ionisation rate coefficient for
-  ! Hydrogen atom, from
-  ! L.C Johnson 
-  ! ApJ 74:227-236, 1972 May 15; eq. 39
-  !
-  ! ne factorised
-  !
-  ! return C(i,j) with j = Nlevel (bound-free)
- ! --------------------------------------------------- !
-   integer, intent(in) :: icell
-   double precision, intent(out), dimension(:) :: Cje
-   integer :: i, j, Nl
-   double precision :: C0, pia0sq, rn, bn, n, An, En, yn, zn, S, Bnp
-   type (AtomType) :: atom
-   
-   atom = atmos%Atoms(1)%ptr_atom
-
-   C0 = dsqrt(8.*KBOLTZMANN*atmos%T(icell) / pi / M_ELECTRON)
-   pia0sq = 2d0 * pi * RBOHR**2
-   
-   Nl = atom%Nlevel
-   !Hydrogen level are ordered by n increasing, except for the continuum level
-   !n = 1., but stops before Nlevel
-   
-   do i=1, Nl-1 !collision from neutral states to the ground state of H+
-    n = real(i,kind=dp)
-    if (i==1) then !n=i
-     rn = 0.45
-     bn = -0.603
-    else 
-     rn = 1.94*n**(-1.57)
-     bn = 1d0/n * (4. - 18.63/n + 36.24/n/n - 28.09/n/n/n)
-    end if
-
-    ! in Joules
-    En = E_RYDBERG / n / n !energie of level with different quantum number in 13.6eV: En = 13.6/n**2
-    yn = En / KBOLTZMANN / atmos%T(icell)
-    An = 32. / 3. / dsqrt(3d0) / pi * n  * (g0(n)/3. + g1(n)/4. + g2(n)/5.)
-    Bnp = 2./3. * n*n * (5. + bn)
-    zn = rn + yn
-
-    S = C0 * pia0sq * (n*yn)**2 * (An*(E1(yn)/yn - E1(zn)/zn) + &
-   			(Bnp - An*log(2*n*n))*(ksi_johnson(yn)-ksi_johnson(zn)))
-   	!!write(*,*) i-1, Nl-1, "S=", S, S*dexp(yn)/dsqrt(atmos%T(icell)) 
-    !check that otherwise multiply by dexp(yn)
-    Cje(i) = S !RH -> dexp(yn) / dsqrt(atmos%T(icell)) !per ne
-    		   !we compute it at icell so in fact we could avoid / sqrt(atmos%icell)
-    		   !as col = Cje * sqrt(T) * exp(-de). Normally dE == En/kT=y so
-    		   !it is not useful to multiply except to take into account the slightly diff
-    		   !between En and (atom%E(Nl)-atom%E(i))
-    !!write(*,*) En, (atom%E(Nl)-atom%E(i)) !Should be similar, because E(j)=13.6/j**2
-   end do
- RETURN
- END SUBROUTINE Johnson_CI
-
- 
- SUBROUTINE Johnson_CE(icell, Cje)
- ! ----------------------------------------------------- !
-  ! Excitation rate coefficient for
-  ! Hydrogen atom, from
-  ! ApJ 74:227-236, 1972 May 15; eq. 36
-  !
-  ! CE = S = C(i,j) all transitions from j, i and i, j
-  ! 
-  ! ( -> transform C(i,j) to specifically C(j,i)
-  ! S * dexp(y) * atom%g(i)/atom%g(j)
-  ! = C(i,j) * exp(hnu/kt)*gi/gj = C(i,j) * ni/nj = C(j,i)
-  ! at LTE: (gi/gj * nj/ni)  = exp(-hnu/kT) )
- ! ----------------------------------------------------- !
-   integer, intent(in) :: icell
-   double precision, intent(out), dimension(:,:) :: Cje
-   integer :: i, j, Nl
-   double precision :: C0, pia0sq, rn, bn, n, Ennp, y, z, S, Bnnp, En
-   double precision :: np, x, fnnp, rnnp, Annp, Gaunt_bf
-   type (AtomType) :: atom
-   
-   atom = atmos%Atoms(1)%ptr_atom
-
-   C0 = dsqrt(8.*KBOLTZMANN*atmos%T(icell) / pi / M_ELECTRON)
-   pia0sq = 2d0 * pi * RBOHR**2
-   
-   Nl = atom%Nlevel
-   !Hydrogen level are ordered by n increasing, except for the continuum level
-   !n = 1., but stops before Nlevel
-   
-   do i=1, Nl-1 !collision between neutral states, n to n'
-    n = real(i,kind=dp)
-    if (i==1) then !n=i
-     rn = 0.45
-     bn = -0.603
-    else 
-     rn = 1.94*n**(-1.57)
-     bn = 1d0/n * (4. - 18.63/n + 36.24/n/n - 28.09/n/n/n)
-    end if
-    
-    do j=i+1, Nl-1
-     np = dble(j)!n'
-     x = 1d0 - (n/np)**2 ! = Enn'/Rdybg
-     !Gauntfactor * 32./3./dsqrt(3.)/pi * n/np**3 /x**3
-     Gaunt_bf = g0(n) + g1(n)/x + g2(n)/x/x
-     fnnp = Gaunt_bf * 32./3./dsqrt(3d0)/pi * n / np / np /np / x / x / x
-     rnnp = rn * x
-     Annp = 2d0 * n*n*fnnp/x
-    ! in Joules
-     En = E_RYDBERG / n / n !energie of level with different quantum number in 13.6eV = ionisation E of n
-     y = x * En / KBOLTZMANN / atmos%T(icell) !x = ratio of E/En
-     Bnnp = 4d0 * (n**4)/(np**3) / x / x * (1. + 4./3. /x + bn/x/x)
-     z = rnnp + y
-   
-     S = C0 * pia0sq * n*n*y*y/x * (Annp*((1./y + 0.5)*E1(y)-(1./z + 0.5)*E1(z))+&
-     	(Bnnp-Annp*dlog(2*n*n/x))*(E2(y)/y - E2(z)/z))
-     
-     Cje(j,i) = S * dexp(y) * atom%g(i)/atom%g(j)
-     !!Cje(i,j) = S
-     !write(*,*) atom%E(j) - atom%E(i), En*x !because x = deltaE/En
-    end do !over np
-   end do  !over n
-
- RETURN
- END SUBROUTINE Johnson_CE
- 
- FUNCTION ksi_johnson(t) result(y)
-  double precision :: t, y
-  !E0
-  y = dexp(-t)/t - 2d0*E1(t) + E2(t)
-   
- RETURN
- END FUNCTION ksi_johnson
- 
- FUNCTION g0 (n) result(g)
-  double precision :: g, n
-  
-  SELECT CASE (int(n))
-  CASE (1)
-   g = 1.1330
-  CASE (2)
-   g = 1.0785
-  CASE DEFAULT
-   g = 0.9935 + 0.2328/n - 0.1296 / n / n
-  END SELECT
- 
- RETURN
- END FUNCTION g0
- 
- FUNCTION g1 (n) result(g)
-  double precision :: g, n
-  
-  SELECT CASE (int(n))
-  CASE (1)
-   g = -0.4059
-  CASE (2)
-   g = -0.2319
-  CASE DEFAULT
-   g = -1d0/n * (0.6282 - 0.5598/n + 0.5299 / n / n)
-  END SELECT  
- 
- RETURN
- END FUNCTION g1
- 
- FUNCTION g2 (n) result(g)
-  double precision :: g, n
- 
-  SELECT CASE (int(n))
-  CASE (1)
-   g = 0.07014
-  CASE (2)
-   g = 0.02947
-  CASE DEFAULT
-   g = 1d0/n/n * (0.3887 - 1.181 / n + 1.470 /n / n)
-  END SELECT  
- 
- RETURN
- END FUNCTION g2
+!  FUNCTION Collision_Hydrogen(icell) result(Cji)
+!   integer :: icell, i, j
+!   double precision :: Cji(Hydrogen%Nlevel, Hydrogen%Nlevel)
+!   double precision :: nr_ji, CI(Hydrogen%Nlevel,Hydrogen%Nlevel), CE(Hydrogen%Nlevel,Hydrogen%Nlevel)
+!   
+!    Cji(:,:) = 0d0; CI = 0d0; CE(:,:) = 0d0 
+!    CALL Johnson_CI(icell, CI(:,Hydrogen%Nlevel)) !bound-free i->Nlevel
+!    CALL Johnson_CE(icell, CE) !among all levels
+! 
+!    do j=1,Hydrogen%Nlevel
+!     do i=1,Hydrogen%Nlevel
+!      nr_ji = Hydrogen%nstar(i,icell)/Hydrogen%nstar(j,icell)
+!      Cji(j,i) = CE(j,i) +  CI(i,j)  * nr_ji
+!      Cji(i,j) = CE(j,i)/nr_ji + CI(i,j)
+!     end do
+!    end do
+!    Cji(:,:) = Cji(:,:) * atmos%ne(icell)
+! 
+!  RETURN
+!  END FUNCTION Collision_Hydrogen
+! 
+!  SUBROUTINE Johnson_CI(icell, Cje)
+!  ! --------------------------------------------------- !
+!   ! Ionisation rate coefficient for
+!   ! Hydrogen atom, from
+!   ! L.C Johnson 
+!   ! ApJ 74:227-236, 1972 May 15; eq. 39
+!   !
+!   ! ne factorised
+!   !
+!   ! return C(i,j) with j = Nlevel (bound-free)
+!  ! --------------------------------------------------- !
+!    integer, intent(in) :: icell
+!    double precision, intent(out), dimension(:) :: Cje
+!    integer :: i, j, Nl
+!    double precision :: C0, pia0sq, rn, bn, n, An, En, yn, zn, S, Bnp
+!    type (AtomType) :: atom
+!    
+!    atom = atmos%Atoms(1)%ptr_atom
+! 
+!    C0 = dsqrt(8.*KBOLTZMANN*atmos%T(icell) / pi / M_ELECTRON)
+!    pia0sq = 2d0 * pi * RBOHR**2
+!    
+!    Nl = atom%Nlevel
+!    !Hydrogen level are ordered by n increasing, except for the continuum level
+!    !n = 1., but stops before Nlevel
+!    
+!    do i=1, Nl-1 !collision from neutral states to the ground state of H+
+!     n = real(i,kind=dp)
+!     if (i==1) then !n=i
+!      rn = 0.45
+!      bn = -0.603
+!     else 
+!      rn = 1.94*n**(-1.57)
+!      bn = 1d0/n * (4. - 18.63/n + 36.24/n/n - 28.09/n/n/n)
+!     end if
+! 
+!     ! in Joules
+!     En = E_RYDBERG / n / n !energie of level with different quantum number in 13.6eV: En = 13.6/n**2
+!     yn = En / KBOLTZMANN / atmos%T(icell)
+!     An = 32. / 3. / dsqrt(3d0) / pi * n  * (g0(n)/3. + g1(n)/4. + g2(n)/5.)
+!     Bnp = 2./3. * n*n * (5. + bn)
+!     zn = rn + yn
+! 
+!     S = C0 * pia0sq * (n*yn)**2 * (An*(E1(yn)/yn - E1(zn)/zn) + &
+!    			(Bnp - An*log(2*n*n))*(ksi_johnson(yn)-ksi_johnson(zn)))
+!    	!!write(*,*) i-1, Nl-1, "S=", S, S*dexp(yn)/dsqrt(atmos%T(icell)) 
+!     !check that otherwise multiply by dexp(yn)
+!     Cje(i) = S !RH -> dexp(yn) / dsqrt(atmos%T(icell)) !per ne
+!     		   !we compute it at icell so in fact we could avoid / sqrt(atmos%icell)
+!     		   !as col = Cje * sqrt(T) * exp(-de). Normally dE == En/kT=y so
+!     		   !it is not useful to multiply except to take into account the slightly diff
+!     		   !between En and (atom%E(Nl)-atom%E(i))
+!     !!write(*,*) En, (atom%E(Nl)-atom%E(i)) !Should be similar, because E(j)=13.6/j**2
+!    end do
+!  RETURN
+!  END SUBROUTINE Johnson_CI
+! 
+!  
+!  SUBROUTINE Johnson_CE(icell, Cje)
+!  ! ----------------------------------------------------- !
+!   ! Excitation rate coefficient for
+!   ! Hydrogen atom, from
+!   ! ApJ 74:227-236, 1972 May 15; eq. 36
+!   !
+!   ! CE = S = C(i,j) all transitions from j, i and i, j
+!   ! 
+!   ! ( -> transform C(i,j) to specifically C(j,i)
+!   ! S * dexp(y) * atom%g(i)/atom%g(j)
+!   ! = C(i,j) * exp(hnu/kt)*gi/gj = C(i,j) * ni/nj = C(j,i)
+!   ! at LTE: (gi/gj * nj/ni)  = exp(-hnu/kT) )
+!  ! ----------------------------------------------------- !
+!    integer, intent(in) :: icell
+!    double precision, intent(out), dimension(:,:) :: Cje
+!    integer :: i, j, Nl
+!    double precision :: C0, pia0sq, rn, bn, n, Ennp, y, z, S, Bnnp, En
+!    double precision :: np, x, fnnp, rnnp, Annp, Gaunt_bf
+!    type (AtomType) :: atom
+!    
+!    atom = atmos%Atoms(1)%ptr_atom
+! 
+!    C0 = dsqrt(8.*KBOLTZMANN*atmos%T(icell) / pi / M_ELECTRON)
+!    pia0sq = 2d0 * pi * RBOHR**2
+!    
+!    Nl = atom%Nlevel
+!    !Hydrogen level are ordered by n increasing, except for the continuum level
+!    !n = 1., but stops before Nlevel
+!    
+!    do i=1, Nl-1 !collision between neutral states, n to n'
+!     n = real(i,kind=dp)
+!     if (i==1) then !n=i
+!      rn = 0.45
+!      bn = -0.603
+!     else 
+!      rn = 1.94*n**(-1.57)
+!      bn = 1d0/n * (4. - 18.63/n + 36.24/n/n - 28.09/n/n/n)
+!     end if
+!     
+!     do j=i+1, Nl-1
+!      np = dble(j)!n'
+!      x = 1d0 - (n/np)**2 ! = Enn'/Rdybg
+!      !Gauntfactor * 32./3./dsqrt(3.)/pi * n/np**3 /x**3
+!      Gaunt_bf = g0(n) + g1(n)/x + g2(n)/x/x
+!      fnnp = Gaunt_bf * 32./3./dsqrt(3d0)/pi * n / np / np /np / x / x / x
+!      rnnp = rn * x
+!      Annp = 2d0 * n*n*fnnp/x
+!     ! in Joules
+!      En = E_RYDBERG / n / n !energie of level with different quantum number in 13.6eV = ionisation E of n
+!      y = x * En / KBOLTZMANN / atmos%T(icell) !x = ratio of E/En
+!      Bnnp = 4d0 * (n**4)/(np**3) / x / x * (1. + 4./3. /x + bn/x/x)
+!      z = rnnp + y
+!    
+!      S = C0 * pia0sq * n*n*y*y/x * (Annp*((1./y + 0.5)*E1(y)-(1./z + 0.5)*E1(z))+&
+!      	(Bnnp-Annp*dlog(2*n*n/x))*(E2(y)/y - E2(z)/z))
+!      
+!      Cje(j,i) = S * dexp(y) * atom%g(i)/atom%g(j)
+!      !!Cje(i,j) = S
+!      !write(*,*) atom%E(j) - atom%E(i), En*x !because x = deltaE/En
+!     end do !over np
+!    end do  !over n
+! 
+!  RETURN
+!  END SUBROUTINE Johnson_CE
+!  
+!  FUNCTION ksi_johnson(t) result(y)
+!   double precision :: t, y
+!   !E0
+!   y = dexp(-t)/t - 2d0*E1(t) + E2(t)
+!    
+!  RETURN
+!  END FUNCTION ksi_johnson
+!  
+!  FUNCTION g0 (n) result(g)
+!   double precision :: g, n
+!   
+!   SELECT CASE (int(n))
+!   CASE (1)
+!    g = 1.1330
+!   CASE (2)
+!    g = 1.0785
+!   CASE DEFAULT
+!    g = 0.9935 + 0.2328/n - 0.1296 / n / n
+!   END SELECT
+!  
+!  RETURN
+!  END FUNCTION g0
+!  
+!  FUNCTION g1 (n) result(g)
+!   double precision :: g, n
+!   
+!   SELECT CASE (int(n))
+!   CASE (1)
+!    g = -0.4059
+!   CASE (2)
+!    g = -0.2319
+!   CASE DEFAULT
+!    g = -1d0/n * (0.6282 - 0.5598/n + 0.5299 / n / n)
+!   END SELECT  
+!  
+!  RETURN
+!  END FUNCTION g1
+!  
+!  FUNCTION g2 (n) result(g)
+!   double precision :: g, n
+!  
+!   SELECT CASE (int(n))
+!   CASE (1)
+!    g = 0.07014
+!   CASE (2)
+!    g = 0.02947
+!   CASE DEFAULT
+!    g = 1d0/n/n * (0.3887 - 1.181 / n + 1.470 /n / n)
+!   END SELECT  
+!  
+!  RETURN
+!  END FUNCTION g2
  
  SUBROUTINE openCollisionFile(atom)
   type (AtomType), intent(in) :: atom
