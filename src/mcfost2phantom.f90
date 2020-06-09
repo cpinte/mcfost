cpinte/mcfost

commit 42263bd081761985396c079d2319a8d57f156d94

manual chi calculation and state dealloaction

src/mcfost2phantom.f90
@@ -137,7 +137,7 @@ contains
 
   subroutine run_mcfost_phantom(&
     np,nptmass,ntypes,ndusttypes,dustfluidtype,npoftype,maxirad,&
-    xyzh,vxyzu,radiation,ichi,&
+    xyzh,vxyzu,radiation,ivorcl,&
     iphase,grainsize,graindens,dustfrac,massoftype,&
     xyzmh_ptmass,hfact,umass,utime,udist,ndudt,dudt,compute_Frad,SPH_limits,&
     Tphantom,n_packets,mu_gas,ierr,write_T_files,ISM,T_to_u)
@@ -146,13 +146,13 @@ contains
     use constantes, only : mu
     use read_phantom
     use stars, only : E_ISM
-    use radiation_field, only : reset_radiation_field, xN_abs
+    use radiation_field, only : xN_abs
     use thermal_emission, only : select_wl_em, repartition_energie, init_reemission, &
-         temp_finale, temp_finale_nlte, repartition_wl_em, set_min_temperature, reset_temperature, E_abs_nRE
-    use mem, only : alloc_dynamique, deallocate_densities
+         temp_finale, temp_finale_nlte, repartition_wl_em, set_min_temperature, E_abs_nRE
+    use mem, only : alloc_dynamique
     use naleat, only : seed, stream, gtype
     use SPH2mcfost, only : SPH_to_Voronoi, compute_stellar_parameters
-    use Voronoi_grid, only : Voronoi, deallocate_Voronoi
+    use Voronoi_grid, only : Voronoi
     use dust_transfer, only : emit_packet, propagate_packet
     use utils, only : progress_bar
     use stars, only : repartition_energie_etoiles, repartition_energie_ISM
@@ -167,7 +167,7 @@ contains
 #include "sprng_f.h"
 
     integer, intent(in) :: np, nptmass, ntypes,ndusttypes,dustfluidtype,&
-       maxirad,ichi
+       maxirad,ivorcl
     real(dp), dimension(4,np), intent(in) :: xyzh,vxyzu
     real(dp), dimension(maxirad,np), intent(inout) :: radiation
     integer(kind=1), dimension(np), intent(in) :: iphase
@@ -196,9 +196,6 @@ contains
     integer, intent(out) :: ierr
 
     real, parameter :: Tmin = 1.
-
-    real(dp), allocatable :: chi_R(:)
-    real(dp) :: default_chi
 
     real(dp), dimension(:), allocatable :: x_SPH,y_SPH,z_SPH,h_SPH,rhogas, massgas, vx_SPH,vy_SPH,vz_SPH, SPH_grainsizes
     integer, dimension(:), allocatable :: particle_id
@@ -302,8 +299,8 @@ contains
 
     call repartition_wl_em()
 
-    letape_th = .true.
-    laffichage=.true.
+    letape_th  = .true.
+    laffichage = .true.
     nbre_phot2 = nbre_photons_eq_th
 
     test_tau : do lambda=1,n_lambda
@@ -349,8 +346,10 @@ contains
           lpacket_alive = .true.
 
           ! Propagation du packet
-          if (lintersect) call propagate_packet(id,lambda,p_lambda,icell,x,y,z,u,v,w,stokes, &
-               flag_star,flag_ISM,flag_scatt,lpacket_alive)
+          if (lintersect) then
+             call propagate_packet(id,lambda,p_lambda,icell,x,y,z,u,v,w,stokes,&
+                flag_star,flag_ISM,flag_scatt,lpacket_alive)
+          endif
        enddo photon !nnfot2
 
        ! Progress bar
@@ -381,39 +380,30 @@ contains
 
 
     call set_min_Temperature(Tmin)
-    allocate(chi_R(n_cells))
-    call diffusion_opacity(chi_R,default_chi)
 
     if (present(write_T_files)) then
        if (write_T_files) call write_mcfost2phantom_temperature()
     endif
-    radiation(ichi,:) = default_chi
     ! SPH particles ignored by mcfost
-    Tphantom = -1.0
+    Tphantom = -1.
     ! Remapping to phantom indices
+    radiation(ivorcl,:) = -1.
     do icell=1, n_cells
        i_SPH = Voronoi(icell)%id
        if (i_SPH > 0) then
           i_Phantom = particle_id(i_SPH)
-          Tphantom(i_Phantom) = Tdust(icell)
+          Tphantom(i_Phantom)  = Tdust(icell)
           n_packets(i_Phantom) = sum(xN_abs(icell,1,:))
-          radiation(ichi,i_Phantom) = chi_R(icell)
+          radiation(ivorcl,i_Phantom) = icell
        endif
     enddo
-
-    call deallocate_Voronoi()
-    call deallocate_densities()
-
-    ! reset energy and temperature arrays
-    call reset_radiation_field()
-    call reset_temperature()
 
     ! Temps d'execution
     call system_clock(time_end)
     if (time_end < time_begin) then
-       time=(time_end + (1.0 * time_max)- time_begin)/real(time_tick)
+       time = (time_end + (1.0 * time_max)- time_begin)/real(time_tick)
     else
-       time=(time_end - time_begin)/real(time_tick)
+       time = (time_end - time_begin)/real(time_tick)
     endif
     if (time > 60) then
        itime = real(time)
@@ -451,6 +441,24 @@ contains
     return
 
   end subroutine run_mcfost_phantom
+
+  subroutine deinit_mcfost_phantom()
+    use Voronoi_grid,     only:deallocate_Voronoi
+    use mem,              only:deallocate_densities
+    use radiation_field,  only:reset_radiation_field
+    use thermal_emission, only:reset_temperature
+
+    call deallocate_Voronoi()
+    call deallocate_densities()
+    ! reset energy and temperature arrays
+    call reset_radiation_field()
+    call reset_temperature()
+    write(*,*)
+    write(*,*) "------------------------------"
+    write(*,*) "MCFOST: Reset State"
+    write(*,*) "------------------------------"
+    write(*,*)
+  end subroutine deinit_mcfost_phantom
 
 !*************************************************************************
 
@@ -490,7 +498,7 @@ contains
 
   !*************************************************************************
 
-  subroutine diffusion_opacity(kappa_diffusion,default_chi)
+  subroutine diffusion_opacity(temp,icell,kappa_diffusion)
     ! Compute current Planck reciprocal mean opacity for all cells
     ! (note : diffusion coefficient needs to be defined with Rosseland opacity in B&W mode)
     ! Diffusion coefficient is D = 1/(rho * opacity)
@@ -504,47 +512,38 @@ contains
     use cylindrical_grid, only : volume
     use density, only : masse_gaz, densite_gaz
 
-    real(dp), allocatable, intent(inout) :: kappa_diffusion(:) ! cm2/g (ie per gram of gas)
-    real(dp), intent(out) :: default_chi
+    real(dp), intent(in)  :: temp
+    integer,  intent(in)  :: icell
+    real(dp), intent(out) :: kappa_diffusion ! cm2/g (ie per gram of gas)
 
-    integer :: icell, lambda
-    real(dp) :: somme, somme2, Temp, cst, cst_wl, B, dB_dT, coeff_exp, wl, delta_wl
+    integer :: lambda
+    real(dp) :: somme, somme2, cst, cst_wl, B, dB_dT, coeff_exp, wl, delta_wl
 
-    default_chi = 0
-
-    do icell=1, n_cells
-       Temp = Tdust(icell)
-       if (Temp > 1) then
-          somme=0.0_dp ; somme2 = 0.0_dp
-
-          cst=cst_th/Temp
-          do lambda=1, n_lambda
-             ! longueur d'onde en metre
-             wl = tab_lambda(lambda)*1.e-6
-             delta_wl=tab_delta_lambda(lambda)*1.e-6
-             cst_wl=cst/wl
-             if (cst_wl < 200.0) then
-                coeff_exp=exp(cst_wl)
-                B = 1.0_dp/((wl**5)*(coeff_exp-1.0))*delta_wl
-                !dB_dT = cst_wl*coeff_exp/((wl**5)*(coeff_exp-1.0)**2)
-             else
-                B = 0.0_dp
-                !dB_dT = 0.0_dp
-             endif
-             somme = somme + B/kappa(icell,lambda) * delta_wl
-             somme2 = somme2 + B * delta_wl
-          enddo
-          kappa_diffusion(icell) = somme2/somme * cm_to_AU  / (densite_gaz(icell) * masse_mol_gaz * (cm_to_m)**3) ! cm^2/g
-          ! check : somme2/somme * cm_to_AU /(masse_gaz(icell)/(volume(icell)*AU_to_cm**3))
-       else
-          kappa_diffusion(icell) = 0
-       endif
-       default_chi = default_chi + kappa_diffusion(icell)
-    enddo ! icell
-    default_chi = default_chi / (n_cells - 1)
-    return
-
+    if (temp > 1) then
+       somme  = 0.0_dp
+       somme2 = 0.0_dp
+       cst    = cst_th/temp
+       do lambda = 1,n_lambda
+          ! longueur d'onde en metre
+          wl       = tab_lambda(lambda)*1.e-6
+          delta_wl = tab_delta_lambda(lambda)*1.e-6
+          cst_wl   = cst/wl
+          if (cst_wl < 200.0) then
+             coeff_exp = exp(cst_wl)
+             B = 1.0_dp/((wl**5)*(coeff_exp-1.0))*delta_wl
+             !dB_dT = cst_wl*coeff_exp/((wl**5)*(coeff_exp-1.0)**2)
+          else
+             B = 0.0_dp
+             !dB_dT = 0.0_dp
+          endif
+          somme  = somme  + B/kappa(icell,lambda)*delta_wl
+          somme2 = somme2 + B*delta_wl
+       enddo
+       kappa_diffusion = somme2/somme&
+          *cm_to_AU/(densite_gaz(icell)*masse_mol_gaz*(cm_to_m)**3) ! cm^2/g
+       ! check : somme2/somme * cm_to_AU /(masse_gaz(icell)/(volume(icell)*AU_to_cm**3))
+    else
+       kappa_diffusion = 0.
+    endif
   end subroutine diffusion_opacity
-
-
 end module mcfost2phantom

commit ab03dd74d903492cfb88157241dc4d8ce240ca37

(phantom) Function to reset radiation arrays was not called
Fixing bug introduced by 42263bd081761985396c079d2319a8d57f156d94

src/mcfost2phantom.f90
@@ -394,6 +394,20 @@ contains
        endif
     enddo
 
+    ! Resetting memory state for next call
+    call reset_mcfost_phantom()
+
+    ! Verifications et codes d'erreur
+    if (maxval(Tphantom) < 0.) then
+       write(*,*) "***********************************************"
+       write(*,*) "ERROR : PB setting T_DUST", n_cells
+       write(*,*)  minval(Tphantom), maxval(Tphantom)
+       write(*,*) "***********************************************"
+
+       ierr = 1
+       return
+    endif
+    
     ! Temps d'execution
     call system_clock(time_end)
     if (time_end < time_begin) then
@@ -416,18 +430,6 @@ contains
     else
        write (*,'(" CPU time used          ", F5.2, "s")')  time
     endif
-
-    ! Verifications et codes d'erreur
-    if (maxval(Tphantom) < 0.) then
-       write(*,*) "***********************************************"
-       write(*,*) "ERROR : PB setting T_DUST", n_cells
-       write(*,*)  minval(Tphantom), maxval(Tphantom)
-       write(*,*) "***********************************************"
-
-       ierr = 1
-       return
-    endif
-
     write(*,*)
     write(*,*) "------------------------------"
     write(*,*) "End of MCFOST run"
@@ -451,7 +453,7 @@ contains
     call deallocate_Voronoi()
     call deallocate_densities()
 
-    ! reset energy and temperature arrays
+    ! Reset energy and temperature arrays
     call reset_radiation_field()
     call reset_temperature()
 
@@ -545,4 +547,5 @@ contains
        kappa_diffusion = 0.
     endif
   end subroutine diffusion_opacity
+  
 end module mcfost2phantom