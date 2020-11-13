eb44136ddaf6bd117f7d32e64983885aab28abc8
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2020-11-13 17:39:24 +0100

    Remove pluto interface to voronoi tesselation: The modifications of the Voronoi grid and on the subroutines associated implie a complete reworking of the old interface

--- src/dust_transfer.f90
+++ src/dust_transfer.f90
@@ -101,8 +101,9 @@ subroutine transfert_poussiere()
      call setup_SPH2mcfost(density_file, limits_file, n_SPH, extra_heating)
      call setup_grid()
   else if (lpluto_file) then
-     CALL setup_pluto_to_mcfost(density_file)
-     CALL setup_grid()
+  	call error("PLUTO interface not handled yet!")
+!      CALL setup_pluto_to_mcfost(density_file)
+!      CALL setup_grid()
   else
      call setup_grid()
      call define_grid() ! included in setup_phantom2mcfost
