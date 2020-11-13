cf80bd920bab3eebded6404a5bc2e59854f3355b
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2020-11-13 16:53:44 +0100

    change subroutine damping() to function

--- src/Atoms/broad.f90
+++ src/Atoms/broad.f90
@@ -68,10 +68,11 @@ MODULE broad
      gamma_j = gamma_j + atom%lines(kp)%Aji
     endif
     
-    if (n==line%i) then !our lower level is also the upper level of other transitions
-!      write(*,*) "i is upper level of ", line%i, n, l
-     gamma_i = gamma_i + atom%lines(kp)%Aji
-    endif
+!only if stm ?
+!     if (n==line%i) then !our lower level is also the upper level of other transitions
+! !      write(*,*) "i is upper level of ", line%i, n, l
+!      gamma_i = gamma_i + atom%lines(kp)%Aji
+!     endif
   
   enddo
   
@@ -92,7 +93,7 @@ MODULE broad
   real(kind=dp), intent(out) :: GvdW
   integer, intent(in) :: icell
   type (AtomType), intent(in) :: atom
-  type (Element), pointer :: Helium
+!   type (Element), pointer :: Helium
   integer, intent(in) :: kr
   integer :: i, j, ic, Z
   real(kind=dp) :: vrel35_H, vrel35_He, deltaR
@@ -361,6 +362,7 @@ MODULE broad
  
  return
  end subroutine resonance_broadening
+ 
 
  SUBROUTINE Damping(icell, atom, kr, adamp)
 
@@ -413,5 +415,233 @@ MODULE broad
 
  RETURN
  END SUBROUTINE Damping
+ 
+ 
+ 
+ 
+
+function line_damping(icell, line)
+
+	integer, intent(in) :: icell
+	type (AtomicLine), intent(in) :: line
+	integer :: k
+	real(kind=dp) :: cDop, adamp
+	real(kind=dp) :: line_damping
+	real(kind=dp) :: Qelast, Gj
+
+
+	line_damping = line%Grad
+
+	!conversion factor from gamma in sr / s^-1 to Gamma in m/s
+	!and then to adamp as Gamma / vrboad
+	cDop = (NM_TO_M*line%lambda0) / (4.*PI)
+  
+
+	! van der Waals broadening
+	!interaction with neutral H and neutral He
+
+	if ((line%cvdWaals(1) > 0).or.(line%cvdWaals(3) > 0)) then
+		CALL VanderWaals_line(icell, line, adamp)
+		line_damping = line_damping + adamp
+	end if
+
+	! Quadratic Stark broadening
+	!Interaction with charged particles
+	!-> for H cStark is 0
+	if (line%cStark /= 0.0) then
+		CALL Stark_line(icell, line, adamp)
+		line_damping = line_damping + adamp
+	end if
+
+	! Linear Stark broadening only for Hydrogen
+	!Approximate treatment at the moment
+	if (line%atom%ID == "H") then
+		CALL StarkLinear_line(icell, line, adamp)
+		line_damping = line_damping + adamp
+
+	end if
+  
+	line_damping = line_damping * cDop / line%atom%vbroad(icell)
+
+
+return
+end function line_damping
+ 
+ SUBROUTINE VanderWaals_line(icell, line, GvdW)
+ ! Compute van der Waals broadening in Lindholm theory with
+ !Unsold's approximation for the interaction coefficient C_6.
+ !
+ !Gamma = 8.08 * vrel^3/5 * C_6^2/5 * nH(1)
+ !
+ !
+ ! Or with the Anstee, Barklem & O'Mara formalism (not yet)
+ !
+ ! See: Anstee & O'Mara 1995, MNRAS 276, 859-866
+ ! Barklem & O'Mara 1998, MNRAS 300, 863-871
+ !
+ ! Gamma =
+ ! (4/pi)^(alpha/2) * &
+ !     Gam((4-alpha)/2) v_0 sigma(v_0)(vmean/v_0)^(1-alpha)
+ !
+  real(kind=dp), intent(out) :: GvdW
+  integer, intent(in) :: icell
+  type (AtomicLine), intent(in) :: line
+  type (Element), pointer :: Helium_elem
+  integer :: i, j, ic, Z, k
+  real(kind=dp) :: vrel35_H, vrel35_He, fourPIeps0, deltaR
+  real(kind=dp) :: cross, gammaCorrH, gammaCorrHe, C625
+
+
+  Helium_elem => Elements(2)%ptr_elem ! Helium basic informations
+
+
+  j = line%j
+  i = line%i
+
+  !could be nice to keep vrel35 for each atom ? as it depends only on the weight
+
+  fourPIeps0 = 4.*PI*EPSILON_0
+  vrel35_He = (8.*KBOLTZMANN/(PI*AMU*line%atom%weight) * (1.+line%atom%weight/Helium_elem%weight))**0.3
+
+  Z = line%atom%stage(j)+1
+  ic = find_continuum(line%atom, j)
+
+  deltaR = (line%atom%Rydberg/(line%atom%E(ic)-line%atom%E(j)))**2. - (line%atom%Rydberg/(line%atom%E(ic)-line%atom%E(i)))**2.
+  C625 = (2.5*((Q_ELECTRON)**2./fourPIeps0)*(ABARH/fourPIeps0) * 2.*PI*(Z*RBOHR)**2./HPLANCK * deltaR)**(4d-1)
+
+
+  SELECT CASE (line%vdWaals)
+  CASE ("UNSOLD")
+   ! Relative velocity of radiator and perturber with Maxwellian
+   ! velocity distributions
+   vrel35_H = (8.*KBOLTZMANN/(PI*AMU*line%atom%weight) * (1.+line%atom%weight/Hydrogen%weight))**(3d-1)
+   cross = 8.08 * (line%cvdWaals(1)*vrel35_H+line%cvdWaals(3)*Helium_elem%abund*vrel35_He)*C625
+   GvdW = cross * T(icell)**(3d-1)
+   
+  CASE ("BARKLEM")
+   write(*,*) "Warning-> vdWaals broadening following BARKLEM ",&
+     "not tested yet!"
+   ! UNSOLD for Helium
+   cross = 8.08 * line%cvdWaals(3)*Helium_elem%abund*vrel35_He*C625
+    GvdW = line%cvdWaals(1) * T(icell)**(real(1.-line%cvdWaals(2),kind=8)/2.) + cross*T(icell)**(3d-1)
+  CASE DEFAULT
+   write(*,*) "Method for van der Waals broadening unknown"
+   write(*,*) "exiting..."
+   stop
+  END SELECT
+
+
+   GvdW = GvdW * Hydrogen%n(1,icell) !total nH_I in the ground state
+
+ RETURN
+ END SUBROUTINE VanderWaals_line
+
+ SUBROUTINE Stark_line(icell,line, GStark)
+ ! Quadratic Stark broadening by electrons and singly charged
+ ! ions.
+ !
+ ! Gamma4 = 11.37 * vrel^1/3 * C_4^2/3 * (ne + nion)
+ !
+ ! Use estimate for C_4 from Traving.
+ !
+ ! See: Traving 1960, "Uber die Theorie der Druckverbreiterung
+ !      von Spektrallinien", p 93
+ !
+ ! -- Mihalas 1978, p. 282ff, and Table 9-1.
+ !
+ ! -- David F. Gray, Observation and Analysis of Stellar
+ !    Photospheres (1992), 2nd ed., p. 216, eq. 11.33
+ !
+ !
+ ! if (line%cStark .lt.0) then Gamma = abs(line%cStark) * ne
+ !
+  real(kind=dp), intent(out) :: GStark
+  integer, intent(in) :: icell
+  type (AtomicLine), intent(in) :: line
+  integer :: k, ic, Z
+  real(kind=dp) :: C4, C, Cm, melONamu, cStark23, cStark
+  real(kind=dp) :: neff_u, neff_l, vrel, ERYDBERG
+
+  melONamu = M_ELECTRON / AMU
+  
+  if ( line%cStark < 0.) then
+   cStark = dabs(real( line%cStark,kind=dp))
+    GStark = cStark * ne(icell)
+  else
+   ! Constants for relative velocity. Assume that nion = ne
+   ! and that the average atomic weight of ionic pertubers is
+   ! given by AVERAGE_ATOMIC_WEIGHT
+
+   C = 8. * KBOLTZMANN / (PI*AMU*line%atom%weight)
+   Cm = (1.+line%atom%weight/melONamu)**(1.6666667d-1) + &
+      (1.+line%atom%weight/AVERAGE_ATOMIC_WEIGHT)**(1.6666667d-1)
+
+   ! Find core charge Z and effective quantumer numbers neff_u
+   ! and neff_l for upper and lower level
+   Z = line%atom%stage( line%i) + 1 !physical, not index
+   ic = find_continuum(line%atom, line%i)
+   
+   ERYDBERG = E_RYDBERG / (1.+M_ELECTRON / (line%atom%weight*AMU)) !corection
+   neff_l = Z*sqrt(ERYDBERG/(line%atom%E(ic)-line%atom%E( line%i)))
+   neff_u = Z*sqrt(ERYDBERG/(line%atom%E(ic)-line%atom%E( line%j)))
+   
+    C4 = ((Q_ELECTRON)**2./(4.*PI*EPSILON_0))*RBOHR *   &
+     (2.*PI*RBOHR**2./HPLANCK) / (18.*Z*Z*Z*Z) * &
+     ((neff_u*(5.0*(neff_u)**2. + 1.0))**2. - (neff_l*(5.0*(neff_l)**2. + 1.0))**2.)
+     
+    cStark23 = 11.37 * ( line%cStark * C4)**(6.6666667d-1)
+
+    vrel = (C*T(icell))**(1.6666667d-1) * Cm
+    GStark = cStark23 * vrel * ne(icell)
+  end if
+
+ RETURN
+ END SUBROUTINE Stark_line
+
+ SUBROUTINE StarkLinear_line(icell, line, GStark)
+ ! Linear Stark broadening by electrons for hydrogen lines.
+ !
+ ! See: K. Sutton (1978), JQSRT 20, 333-343 (formula for z = nu)
+ !
+ ! GStark = a_1 * &
+ !       [0.60 * (n_u^2 - n_l^2) * (N_e)^(2/3) * CM_TO_M^2] !s^-1, with N_e here in cm^-3
+ !
+ ! This is highly inaccurate for lines with n > 2
+ !
+  real(kind=dp), intent(out) :: GStark
+  integer, intent(in) :: icell
+  type (AtomicLine), intent(in) :: line
+  real(kind=dp) :: n_upper, n_lower, Gstark2
+  real :: Z, K, dn, a1, gs
+  real(kind=dp) :: C, nelectric
+  
+  Z = 1.0 + line%atom%stage(line%i)
+  
+  !include proton ?
+  nelectric = 1d-6 * (ne(icell) + 0.0 * hydrogen%n(hydrogen%Nlevel,icell)) !cm^-3
+
+  n_lower = sqrt(line%atom%g(line%i)/2.)
+  n_upper = sqrt(line%atom%g(line%j)/2.) !works because stark linear only for Hydrogen.
+  									 !at the moment. otherwise use n_eff, wich works for H
+  									 
+  dn = real(n_upper - n_lower)
+
+  if (dn == 1.0) then !first line of the serie, dn=1
+   a1 = 0.642
+  else
+   a1 = 1.0
+  end if
+  
+  !Sutton recommends to multiply dz with gs=0.425 if to be used with a dispersion profile
+  !but since it is very inaccurate and we miss broadening 
+  gs = 1.0 !
+
+  GStark = gs * a1 * 0.6 * (n_upper*n_upper-n_lower*n_lower) * ( nelectric**(2./3.) )
+
+
+
+ RETURN
+ END SUBROUTINE StarkLinear_line
+ 
 
 END MODULE broad
