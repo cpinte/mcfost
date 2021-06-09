25030b7abda3bfb7a89a8345847dc957992683e3
Daniel Price <daniel.price@monash.edu>
2021-06-09 11:10:39 +1000

    created hydro_to_voronoi_atomic for common functionality needed in pluto/phantom data reads
    - call this from setup_mhd_to_mcfost, contents identical to previous
    - also call this from setup_SPH2mcfost, handle case where mass_ne_on_massgas and mask are not allocated

--- src/Atoms/read_pluto.f90
+++ src/Atoms/read_pluto.f90
@@ -1,17 +1,11 @@
 module pluto_mod
-  !use density, only : densite_gaz, masse_gaz !used to build nHtot
   use parametres
   use messages
-  use constantes, only : masseH
   use getline
-  use atmos_type, only : alloc_atomic_atmos, alloc_magnetic_field, wght_per_H, nHtot, ne, &
-       v_char, lmagnetized, vturb, T, icompute_atomRT, calc_ne, laccretion_shock, &
-       Taccretion, write_atmos_domain
+  use atmos_type, only : lmagnetized, laccretion_shock, Taccretion
 
   use utils
-  use Voronoi_grid, only : Voronoi, volume, neighbours_list
-  use sph2mcfost, only : SPH_to_Voronoi
-  ! 	use grid,
+  use sph2mcfost, only : SPH_to_Voronoi, Hydro_to_Voronoi_atomic
 
   implicit none
 
@@ -20,25 +14,23 @@ contains
 
   subroutine setup_mhd_to_mcfost()
     !here lmhd_voronoi ist true even with model ascii !
-    integer										:: n_points, i, j, k, icell
-    integer										:: Nread
-    real(kind=dp), allocatable, dimension(:)	:: x,y,z,h!allocated in reading
-    real(kind=dp), allocatable, dimension(:)	:: vx,vy,vz,mass_gas, mass_ne_on_massgas, T_tmp, vt_tmp, dz
-    real(kind=dp), allocatable, dimension(:)	:: rho, hydro_grainsizes
-    real(kind=dp), allocatable, dimension(:,:)	:: rhodust, massdust
-    logical, allocatable, dimension(:)			:: mask
+    integer                                  :: n_points,k,icell
+    integer                                  :: Nread
+    real(kind=dp), allocatable, dimension(:) :: x,y,z,h!allocated in reading
+    real(kind=dp), allocatable, dimension(:) :: vx,vy,vz,mass_gas, mass_ne_on_massgas, T_tmp, vt_tmp, dz
+    real(kind=dp), allocatable, dimension(:) :: rho, hydro_grainsizes
+    real(kind=dp), allocatable, dimension(:,:) :: rhodust, massdust
 
-    real(kind=dp)								:: thetai, thetao, tilt
-    integer, parameter							:: Nheader = 3 !Add more, for ascii file
-    character(len=MAX_LENGTH)					:: rotation_law
-    integer										:: syst_status, acspot, alloc_status
-    character(len=MAX_LENGTH)					:: inputline, FormatLine, cmd
-    real(kind=dp)								:: rho_to_nH, vxmax, vxmin, vymax, vymin, vzmax, vzmin, vmax
-    logical										:: check_previous_tesselation
-    integer,  allocatable, dimension(:)			:: particle_id
-    real(kind=dp), dimension(6)					:: hydro_limits
-    integer										:: ndusttypes, voroindex, N_fixed_ne = 0
-    real, parameter								:: limit_factor = 1.005, Lextent = 1.01
+    real(kind=dp)                            :: thetai, thetao, tilt
+    integer, parameter                       :: Nheader = 3 !Add more, for ascii file
+    character(len=MAX_LENGTH)                :: rotation_law
+    integer                                  :: syst_status, acspot, alloc_status
+    character(len=MAX_LENGTH)                :: inputline, FormatLine, cmd
+    logical                                  :: check_previous_tesselation
+    integer,  allocatable, dimension(:)      :: particle_id
+    real(kind=dp), dimension(6)              :: hydro_limits
+    integer                                  :: ndusttypes !, voroindex, N_fixed_ne = 0
+    real, parameter                          :: limit_factor = 1.005!, Lextent = 1.01
 
     write(FormatLine,'("(1"A,I3")")') "A", MAX_LENGTH
 
@@ -53,6 +45,7 @@ contains
     lignore_dust = .true.
     lrandomize_voronoi = .false.
     check_previous_tesselation = (.not.lrandomize_voronoi)
+    n_points = 0 ! to avoid compiler warning
 
     if (lignore_dust) then
        ndusttypes = 0
@@ -193,113 +186,10 @@ contains
     !     	deallocate(massdust,rhodust,hydro_grainsizes)
     !** bug handling
 
-    !*******************************
-    ! Accomodating model
-    !*******************************
-    !alloc space for all physical quantities.
-    !Velocity field arrays not allocated because lvoronoi is true
-    !-> fills element abundances structures for elements
-    call alloc_atomic_atmos
-    rho_to_nH = 1d3 / masseH / wght_per_H !convert from density to number density nHtot
-
-    Vxmax = 0
-    Vymax = 0
-    Vzmax = 0
-    Vxmin = 1d100
-    Vymin = 1d100
-    Vzmin = 1d00
-    Vmax = 0
-    icompute_atomRT(:) = 0
-    do icell=1,n_cells
-       voroindex = Voronoi(icell)%id
-       if (voroindex > 0) then
-          nHtot(icell)  = Msun_to_kg * rho_to_nH * mass_gas(voroindex) /  (volume(icell) * AU3_to_m3)
-
-          !store the ratio of mass electron on mass hydrogen
-          !to do the tesselation only on gas particle
-          !-> if ne is not present in the model, it is simply 0.
-          ne(icell) = Msun_to_kg * mass_ne_on_massgas(voroindex) * mass_gas(voroindex) / (volume(icell) * AU3_to_m3)
-
-          T(icell) = T_tmp(voroindex)
-
-          vturb(icell) = vt_tmp(voroindex)
-
-          icompute_atomRT(icell) = dz(voroindex)
-
-          vxmax = max(vxmax, abs(Voronoi(icell)%vxyz(1)))
-          vxmin = min(vxmin, max(abs(Voronoi(icell)%vxyz(1)),0.0))
-          vymax = max(vymax, abs(Voronoi(icell)%vxyz(2)))
-          vymin = min(vymin,  max(abs(Voronoi(icell)%vxyz(2)),0.0))
-          vzmax = max(vzmax, abs(Voronoi(icell)%vxyz(3)))
-          vzmin = min(vzmin,  max(abs(Voronoi(icell)%vxyz(3)),0.0))
-
-          Vmax = max( vmax, sqrt(Voronoi(icell)%vxyz(1)**2 + Voronoi(icell)%vxyz(2)**2 + Voronoi(icell)%vxyz(3)**2) )
-       endif
-    end do
+    call hydro_to_Voronoi_atomic(n_points,T_tmp,vt_tmp,mass_gas,mass_ne_on_massgas,dz)
 
     !deallocating temporary variables from input file.
     deallocate(h,vx,vy,vz,mass_gas, mass_ne_on_massgas, x,y,z,T_tmp, vt_tmp, dz)
-
-    v_char = Vmax * Lextent
-
-
-    write(*,*) "Read ", size(pack(icompute_atomRT,mask=icompute_atomRT>0)), " density zones"
-    write(*,*) "Read ", size(pack(icompute_atomRT,mask=icompute_atomRT==0)), " transparent zones"
-    write(*,*) "Read ", size(pack(icompute_atomRT,mask=icompute_atomRT<0)), " dark zones"
-
-
-
-    ! 		if (lmagnetized) then
-    !
-    ! 		endif
-
-
-    calc_ne = .false.
-    icell_loop : do icell=1,n_cells
-       !check that in filled cells there is electron density otherwise we need to compute it
-       !from scratch.
-       if (icompute_atomRT(icell) > 0) then
-
-          if (ne(icell) <= 0.0_dp) then
-             write(*,*) "  ** No electron density found in the model! ** "
-             calc_ne = .true.
-             exit icell_loop
-          endif
-
-       endif
-    enddo icell_loop
-    N_fixed_ne = size(pack(icompute_atomRT,mask=(icompute_atomRT==2)))
-    if (N_fixed_ne > 0) then
-       write(*,'("Found "(1I5)" cells with fixed electron density values! ("(1I3)" %)")') &
-            N_fixed_ne, nint(real(N_fixed_ne) / real(n_cells) * 100)
-    endif
-
-    call write_atmos_domain() !but for consistency with the plot functions in python
-
-
-    write(*,*) "Maximum/minimum velocities in the model (km/s):"
-    write(*,*) "|Vx|", vxmax*1d-3, vxmin*1d-3
-    write(*,*) "|Vy|", vymax*1d-3, vymin*1d-3
-    write(*,*) "|Vz|", vzmax*1d-3, vzmin*1d-3
-
-
-
-    write(*,*) "Typical line extent due to V fields (km/s):"
-    write(*,*) v_char/1d3
-
-    write(*,*) "Maximum/minimum turbulent velocity (km/s):"
-    write(*,*) maxval(vturb)/1d3, minval(vturb, mask=icompute_atomRT>0)/1d3
-
-
-    write(*,*) "Maximum/minimum Temperature in the model (K):"
-    write(*,*) MAXVAL(T), MINVAL(T,mask=icompute_atomRT>0)
-    write(*,*) "Maximum/minimum Hydrogen total density in the model (m^-3):"
-    write(*,*) MAXVAL(nHtot), MINVAL(nHtot,mask=icompute_atomRT>0)
-    if (.not.calc_ne) then
-       write(*,*) "Maximum/minimum ne density in the model (m^-3):"
-       write(*,*) MAXVAL(ne), MINVAL(ne,mask=icompute_atomRT>0)
-    endif
-
 
     return
   end subroutine setup_mhd_to_mcfost
