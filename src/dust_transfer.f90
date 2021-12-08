0d6f206d1de98d87fe87fceee44ecb69d495e555
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2021-12-09 08:44:03 +1100

    Removing teporary test

--- src/dust_transfer.f90
+++ src/dust_transfer.f90
@@ -62,7 +62,6 @@ subroutine transfert_poussiere()
   logical :: flag_star, flag_scatt, flag_ISM
 
   logical :: laffichage, flag_em_nRE, lcompute_dust_prop
-  logical :: lvoro_star
 
   ! Paramètres parallelisation
   integer :: id=1
@@ -94,19 +93,8 @@ subroutine transfert_poussiere()
 
   ! Building the dust grain population
   call build_grain_size_distribution()
-  
-  lvoro_star = .true.
 
   if (lphantom_file .or. lgadget2_file .or. lascii_SPH_file) then
-  	 if (lvoro_star) then
-  	 	llimits_file = .false.
-  	 	limits_file = ""
-  	 	density_file="test_voro_star.sph"
-  	 	lascii_sph_file = .true.
-  	 	lphantom_file = .false.
-  	 	lgadget2_file = .false.
-  	 	lignore_dust = .true.
-  	 endif
      call setup_SPH2mcfost(density_file, limits_file, n_SPH, extra_heating)
      call setup_grid()
   else
