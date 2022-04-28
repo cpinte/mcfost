2e365712a2d41b5cb74c51e6e019d6bc6902d837
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2022-04-28 16:22:48 +1000

    Small edit in code to prepare for astrochem

--- src/for_astrochem.f90
+++ src/for_astrochem.f90
@@ -2,19 +2,21 @@
 
   n_cells ! size(data_grid)
 
-  call compute_column(1, column)
+  call compute_column(1, column) ! particle/cm^-2
 
-  G(:) = compute_UV_field()
+  G(:) = compute_UV_field() ! Habing
 
   ! unit conversions for astrochem :
   gas_density(:) = densite_gaz(1:n_cells) * masse_mol_gaz / m3_to_cm3 ! nH2/m**3 --> g/cm**3
 
   do icell=1,n_cells
      dust_mass_density(icell) = sum(densite_pouss(:,icell) * M_grain(:)) ! M_grain en g
-     dust_particle_dens(icell,:) = densite_pouss(:,icell) * m3_to_cm3
+     dust_particle_dens(icell,:) = densite_pouss(:,icell) * m3_to_cm3    ! particle/cm^-3
   enddo ! icell
 
   ! No conversion
-  Tdust(1:n_cells)
+  Tdust(1:n_cells)  ! K
 
-  r_grain
+  r_grain ! in micron
+
+  r_grid ! in au
