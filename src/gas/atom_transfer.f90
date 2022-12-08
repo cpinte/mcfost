f4392b7f475af0e70879f5c912ca1ae85ab8a074
Benjamin Tessore <benjamin.tessore@univ-grenoble-alpes.fr>
2022-12-08 11:58:21 +0100

    play on the activation of Ng's acc.

--- src/gas/atom_transfer.f90
+++ src/gas/atom_transfer.f90
@@ -338,9 +338,13 @@ module atom_transfer
             ! **********  Ng's acceleration administration *************!
             if (lng_acceleration) then
           	!be sure we are converging before extrapolating
-               lconverging = (conv_speed < 0) .and. (-conv_speed < conv_speed_limit)
+               if (dpops_max_error >= 1d-2) then
+                  lconverging = (conv_speed < 0) .and. (-conv_speed < conv_speed_limit)
+               else
+                  lconverging = (diff_old < 5d-2); Ng_Nperiod = 0
+               endif
                !or if the number of iterations is too large
-               lconverging = lconverging .or. (n_iter > int(real(maxIter)/3.0))
+               lconverging = lconverging .or. (n_iter > int(real(maxIter)/3.0)) !futur deprec of this one (non-local op)
           	   if ( (n_iter>max(Ng_Ndelay_init,1)).and.lconverging ) then
           		   if (.not.lng_turned_on) then
                      write(*,*) " +++ Activating Ng's acceleration +++ "
