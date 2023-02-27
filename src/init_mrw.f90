40c25659fbe6f0e27d1b54859e7bb7834b689bac
Sahl Rowther <phrfmn@login1.orac.cluster>
2023-02-27 11:54:42 +0000

    Edited to allow it to be imported as a module

--- src/init_mrw.f90
+++ src/init_mrw.f90
@@ -1,4 +1,13 @@
-program initialize_cumulative_zeta
+module zeta_MRW
+    use mcfost_env, only : dp
+    implicit none
+
+    integer, parameter :: n = 10000
+    real(dp), dimension(n) :: zeta, y_MRW 
+
+contains
+
+ subroutine initialize_cumulative_zeta()
   ! Compute equation (7) of Min et al (2009):
   ! P(t) = 2 * Sum_{n=1}^{infinity} (-1)^(n+1) * y^(n^2)
   !
@@ -7,39 +16,35 @@ program initialize_cumulative_zeta
 
   implicit none
 
-  integer, parameter :: sp = selected_real_kind(p=6,r=37)
-  integer, parameter :: dp = selected_real_kind(p=13,r=200)
-
-  integer, parameter :: n = 10000
-
-  real(kind=8), dimension(n) :: x, y
   integer :: i,j
   real(kind=8) :: term
 
-  y = 0._dp
+  zeta = 0._dp
 
   do i=1,n
-     x(i) = real(i-1, dp)/real(n-1, dp)
+     y_MRW(i) = real(i-1, dp)/real(n-1, dp)
 
      if(i==n) then
-        y(i) = 0.5_dp
+        zeta(i) = 0.5_dp
      else
         j = 0
         do
            j = j + 1
-           term = x(i)**(j**2)
+           term = y_MRW(i)**(j**2)
            if(term == 0._dp) exit
            if(mod(j, 2)==0) then
-              y(i) = y(i) - term
+              zeta(i) = zeta(i) - term
            else
-              y(i) = y(i) + term
+              zeta(i) = zeta(i) + term
            end if
         end do
      end if
 
-        write(*,*) i, j, x(i), 2*y(i)
+!        write(*,*) i, j, y_MRW(i), 2*zeta(i)
      end do
 
-  y = y * 2._dp
+  zeta = zeta * 2._dp
 
-end program initialize_cumulative_zeta
+ end subroutine initialize_cumulative_zeta
+
+end module zeta_MRW
