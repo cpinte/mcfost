0b4ed3c32d0143f0249c8b28f0d356ef9243bbae
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2024-01-18 23:43:29 +1100

    Decreasing pola threshold

--- test_suite/test_mcfost.py
+++ test_suite/test_mcfost.py
@@ -194,7 +194,7 @@ def test_pola(model_name, wl):
     if model_name == "debris":
         mask_threshold = 1e-32
     elif model_name == "ref3.0_multi":
-        mask_threshold = 1e-19
+        mask_threshold = 1e-22
     else:
         mask_threshold = 1e-21
 
