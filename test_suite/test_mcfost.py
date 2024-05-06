5ff77afc3c09149e2f7b4a0795ea90501be813af
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2024-05-06 14:37:11 +1000

    Trying to debug ref3.0 test failure

--- test_suite/test_mcfost.py
+++ test_suite/test_mcfost.py
@@ -15,7 +15,7 @@ model_list = glob.glob1("test_data/","*")
 #if os.environ.get('CI', None) == 'true':
 #    model_list = ["ref3.0","ref3.0_multi","debris","discF_00500"]
 
-#model_list = ["ref3.0_multi"]#,"debris","discF_00500"]
+model_list = ["ref3.0"]#,"debris","discF_00500"]
 
 wl_list = ["1.0","10","100","1000"]
 wl_list_pola = ["1.0","1000"]
@@ -159,15 +159,30 @@ def test_image(model_name, wl):
 
     # Read the results
     image_name = model_name+"/data_"+wl+"/RT.fits.gz"
+
+
+
+    # We just keep intensity
+    for i in range(5):
+        image = fits.getdata(test_dir+"/"+image_name)
+        image_ref = fits.getdata("test_data/"+image_name)
+
+        print(image.shape)
+        print(image_ref.shape)
+
+        image = image[0,:,i,:,:]
+        image_ref = image_ref[0,:,i,:,:]
+
+        print("Maximum image difference", i, (abs(image-image_ref)/(image_ref+1e-30)).max())
+        print("Mean image difference   ", i, (abs(image-image_ref)/(image_ref+1e-30)).mean())
+
+        print("MC similar", MC_similar(image_ref,image,threshold=0.1))
+
     image = fits.getdata(test_dir+"/"+image_name)
     image_ref = fits.getdata("test_data/"+image_name)
 
-    # We just keep intensity
     image = image[0,:,:,:,:]
     image_ref = image_ref[0,:,:,:,:]
-
-    print("Maximum image difference", (abs(image-image_ref)/(image_ref+1e-30)).max())
-    print("Mean image difference   ", (abs(image-image_ref)/(image_ref+1e-30)).mean())
 
     assert MC_similar(image_ref,image,threshold=0.1)
 
