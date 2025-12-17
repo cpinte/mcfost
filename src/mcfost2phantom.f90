0a849d8d7082719ae1a91fdd3dc8e61c65826462
Rebecca Nealon <rebecca.nealon@warwick.ac.uk>
2025-12-17 16:00:15 +1100

    (apr) allocation of n_cells_max takes into account apr

--- src/mcfost2phantom.f90
+++ src/mcfost2phantom.f90
@@ -159,7 +159,7 @@ contains
   subroutine run_mcfost_phantom(np,nptmass,ntypes,ndusttypes,dustfluidtype, &
        npoftype,xyzh,vxyzu,iphase,grainsize,graindens,dustfrac,massoftype,&
        xyzmh_ptmass,vxyz_ptmass,hfact,umass,utime,udist,ndudt,dudt,compute_Frad,SPH_limits,&
-       Tphantom, n_packets,mu_gas,ierr,write_T_files,ISM,T_gas,apr_level)
+       Tphantom, n_packets,mu_gas,ierr,write_T_files,ISM,T_gas,apr_level,use_apr)
 
     use parametres
     use constantes, only : mu
@@ -204,6 +204,7 @@ contains
     logical, intent(in), optional :: write_T_files
 
     logical, intent(in) :: compute_Frad ! does mcfost need to compute the radiation pressure
+    logical, intent(in) :: use_apr
     real(dp), dimension(6), intent(in) :: SPH_limits ! not used yet, llimits_file is set to false
 
     integer, intent(in) :: ndudt
@@ -275,7 +276,11 @@ contains
 
     ! We allocate the total number of SPH cells as the number of Voronoi cells mays vary
     if (lfirst_time) then
-       call alloc_dynamique(n_cells_max = n_SPH + n_etoiles)
+       if (use_apr) then
+          call alloc_dynamique(n_cells_max = n_SPH*8 + n_etoiles) ! this is the value currently used
+       else                                                       ! in Phantom (config.F90)
+          call alloc_dynamique(n_cells_max = n_SPH + n_etoiles)
+       endif
        alloc_status = 0
        allocate(xN_abs(n_SPH + n_etoiles,1,nb_proc),  stat=alloc_status)
        if (alloc_status /= 0) call error("Allocation error xN_abs")
