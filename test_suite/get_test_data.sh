d5628fcfe09a973563b5a8bb3ee76d061ca0fb68
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2026-01-19 12:11:20 +1100

    Updating get_data.sh

--- test_suite/get_test_data.sh
+++ test_suite/get_test_data.sh
@@ -1,5 +1,11 @@
-wget -nv https://ipag.osug.fr/~pintec/mcfost/test_data_0c484a1a905c13bea4c3bc0494ba288daddbe779.tgz
-rm -rf test_data
-tar xzf test_data_0c484a1a905c13bea4c3bc0494ba288daddbe779.tgz
+#wget -nv https://ipag.osug.fr/~pintec/mcfost/test_data_0c484a1a905c13bea4c3bc0494ba288daddbe779.tgz
 
+wget  -O test_data_ca20c69767a8d3bc8da2831d258424b4ae851ab3.tgz --no-check-certificate  "https://drive.usercontent.google.com/download?export=download&id=1_slLnqLifQJzHpTrpbuCYVyUoGFpDqoZ&confirm=t"
+#https://drive.google.com/file/d/1_slLnqLifQJzHpTrpbuCYVyUoGFpDqoZ/view?usp=drive_link
+rm -rf test_data
+tar xzf test_data_ca20c69767a8d3bc8da2831d258424b4ae851ab3.tgz
+
+
+#Previous versions:
+#test_data_0c484a1a905c13bea4c3bc0494ba288daddbe779.tgz
 #test_data_b17a7fd4bce16a7c2befe713f6dd91773c057740.tar.gz
